4845c16859a4b047d58d88de37cf8e3163f8d034
Arve Hjønnevåg <arve@android.com>
2009-09-21 17:26:47 -0700

    power_supply: Hold a wake_lock while power supply change notifications are pending
    When connecting usb or the charger the device would often go back to sleep
    before the charge led and screen turned on.
    
    Change-Id: I01def6d86ddece0d4e31d2a91d176ed0975b6b9d
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -247,6 +247,9 @@ struct power_supply {
 	/* private */
 	struct device *dev;
 	struct work_struct changed_work;
+	spinlock_t changed_lock;
+	bool changed;
+	struct wake_lock work_wake_lock;
 #ifdef CONFIG_THERMAL
 	struct thermal_zone_device *tzd;
 	struct thermal_cooling_device *tcd;
