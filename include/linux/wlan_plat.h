41bc34a917af5abf0c896725e594215c140c73ea
Dmitry Shmidt <dimitrysh@google.com>
2010-03-18 16:04:18 -0700

    wlan: Extract generic wlan platform data from tiwlan specific header
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wlan_plat.h
+++ include/linux/wlan_plat.h
@@ -16,16 +16,10 @@
 #define _LINUX_WLAN_PLAT_H_
 
 struct wifi_platform_data {
-#if defined(CONFIG_BCM4335) || defined(CONFIG_BCM4335_MODULE)
-	int (*set_power)(int val,bool b0rev);
-#else
 	int (*set_power)(int val);
-#endif
 	int (*set_reset)(int val);
 	int (*set_carddetect)(int val);
 	void *(*mem_prealloc)(int section, unsigned long size);
-	int (*get_mac_addr)(unsigned char *buf);
-	void *(*get_country_code)(char *ccode);
 };
 
 #endif
