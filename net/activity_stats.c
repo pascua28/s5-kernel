772c72a0e60fe35ad63036ec4cfe593c9b25b2c6
Mike Chan <mike@android.com>
2010-05-28 14:32:19 -0700

    net: activity_stats: Add statistics for network transmission activity
    When enabled, tracks the frequency of network transmissions
    (inbound and outbound) and buckets them accordingly.
    Buckets are determined by time between network activity.
    
    Each bucket represents the number of network transmisions that were
    N sec or longer apart. Where N is defined as 1 << bucket index.
    
    This network pattern tracking is particularly useful for wireless
    networks (ie: 3G) where batching network activity closely together
    is more power efficient than far apart.
    
    New file: /proc/net/stat/activity
    
    output:
    
    Min Bucket(sec) Count
                  1 7
                  2 0
                  4 1
                  8 0
                 16 0
                 32 2
                 64 1
                128 0
    
    Change-Id: I4c4cd8627b872a55f326b1715c51bc3bdd6e8d92
    Signed-off-by: Mike Chan <mike@android.com>

--- net/activity_stats.c
+++ net/activity_stats.c
@@ -13,10 +13,11 @@
  *
  * Author: Mike Chan (mike@android.com)
  */
+
 #include <linux/proc_fs.h>
-#include <linux/seq_file.h>
 #include <linux/suspend.h>
 #include <net/net_namespace.h>
+
 /*
  * Track transmission rates in buckets (power of 2).
  * 1,2,4,8...512 seconds.
@@ -25,20 +26,24 @@
  * N seconds apart, where N is 1 << bucket index.
  */
 #define BUCKET_MAX 10
+
 /* Track network activity frequency */
 static unsigned long activity_stats[BUCKET_MAX];
 static ktime_t last_transmit;
 static ktime_t suspend_time;
 static DEFINE_SPINLOCK(activity_lock);
+
 void activity_stats_update(void)
 {
 	int i;
 	unsigned long flags;
 	ktime_t now;
 	s64 delta;
+
 	spin_lock_irqsave(&activity_lock, flags);
 	now = ktime_get();
 	delta = ktime_to_ns(ktime_sub(now, last_transmit));
+
 	for (i = BUCKET_MAX - 1; i >= 0; i--) {
 		/*
 		 * Check if the time delta between network activity is within the
@@ -46,24 +51,39 @@ void activity_stats_update(void)
 		 */
 		if (delta < (1000000000ULL << i))
 			continue;
+
 		activity_stats[i]++;
 		last_transmit = now;
 		break;
 	}
 	spin_unlock_irqrestore(&activity_lock, flags);
 }
-static int activity_stats_show(struct seq_file *m, void *v)
+
+static int activity_stats_read_proc(char *page, char **start, off_t off,
+					int count, int *eof, void *data)
 {
 	int i;
-	int ret;
-	seq_printf(m, "Min Bucket(sec) Count\n");
+	int len;
+	char *p = page;
+
+	/* Only print if offset is 0, or we have enough buffer space */
+	if (off || count < (30 * BUCKET_MAX + 22))
+		return -ENOMEM;
+
+	len = snprintf(p, count, "Min Bucket(sec) Count\n");
+	count -= len;
+	p += len;
+
 	for (i = 0; i < BUCKET_MAX; i++) {
-		ret = seq_printf(m, "%15d %lu\n", 1 << i, activity_stats[i]);
-		if (ret)
-			return ret;
+		len = snprintf(p, count, "%15d %lu\n", 1 << i, activity_stats[i]);
+		count -= len;
+		p += len;
 	}
-	return 0;
+	*eof = 1;
+
+	return p - page;
 }
+
 static int activity_stats_notifier(struct notifier_block *nb,
 					unsigned long event, void *dummy)
 {
@@ -71,29 +91,25 @@ static int activity_stats_notifier(struct notifier_block *nb,
 		case PM_SUSPEND_PREPARE:
 			suspend_time = ktime_get_real();
 			break;
+
 		case PM_POST_SUSPEND:
 			suspend_time = ktime_sub(ktime_get_real(), suspend_time);
 			last_transmit = ktime_sub(last_transmit, suspend_time);
 	}
+
 	return 0;
 }
-static int activity_stats_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, activity_stats_show, PDE_DATA(inode));
-}
-static const struct file_operations activity_stats_fops = {
-	.open		= activity_stats_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= seq_release,
-};
+
 static struct notifier_block activity_stats_notifier_block = {
 	.notifier_call = activity_stats_notifier,
 };
+
 static int  __init activity_stats_init(void)
 {
-	proc_create("activity", S_IRUGO,
-		    init_net.proc_net_stat, &activity_stats_fops);
+	create_proc_read_entry("activity", S_IRUGO,
+			init_net.proc_net_stat, activity_stats_read_proc, NULL);
 	return register_pm_notifier(&activity_stats_notifier_block);
 }
+
 subsys_initcall(activity_stats_init);
+
