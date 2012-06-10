9c914470c646c687049f1e5a4602a3cc297ce4be
Al Viro <viro@zeniv.linux.org.uk>
2012-06-10 18:05:36 -0400

    don't pass nameidata to ->create()
    boolean "does it have to be exclusive?" flag is passed instead;
    Local filesystem should just ignore it - the object is guaranteed
    not to be there yet.
    
    Change-Id: I25efea9892458f6f64070c62bd1adb5194dcd8c1
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ecryptfs/inode.c
+++ fs/ecryptfs/inode.c
@@ -419,7 +419,6 @@ int ecryptfs_check_subfs(struct dentry *de, struct nameidata *nd, char *fs)
  * @dir: The inode of the directory in which to create the file.
  * @dentry: The eCryptfs dentry
  * @mode: The mode of the new file.
- * @nd: nameidata
  *
  * Creates a new file.
  *
@@ -427,7 +426,7 @@ int ecryptfs_check_subfs(struct dentry *de, struct nameidata *nd, char *fs)
  */
 static int
 ecryptfs_create(struct inode *directory_inode, struct dentry *ecryptfs_dentry,
-		umode_t mode, struct nameidata *nd)
+		umode_t mode, bool excl)
 {
 	struct inode *ecryptfs_inode;
 	int rc;
