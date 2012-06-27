33548ab446ed3c877f347403a1090058146274f9
Rafael J. Wysocki <rjw@sisk.pl>
2012-06-27 23:25:38 +0200

    ACPI / PM: Drop pm_message_t argument from device suspend callback
    None of the drivers implementing the ACPI device suspend callback
    uses the pm_message_t argument of it, so this argument may be dropped
    entirely from that callback.  This will simplify switching the ACPI
    bus type to PM handling based on struct dev_pm_ops.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/acpi/processor_idle.c
+++ drivers/acpi/processor_idle.c
@@ -240,7 +240,7 @@ static void acpi_idle_bm_rld_restore(void)
 		acpi_write_bit_register(ACPI_BITREG_BUS_MASTER_RLD, saved_bm_rld);
 }
 
-int acpi_processor_suspend(struct acpi_device * device, pm_message_t state)
+int acpi_processor_suspend(struct acpi_device * device)
 {
 	acpi_idle_bm_rld_save();
 	return 0;
