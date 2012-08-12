1c2357b5001a7e11ead3a2042a077af1db296197
Al Viro <viro@zeniv.linux.org.uk>
2012-08-12 16:17:59 -0400

    take rlimit check to callers of expand_files()
    ... except for one in android, where the check is different
    and already done in caller.  No need to recalculate rlimit
    many times in alloc_fd() either.
    
    Change-Id: Ia6eb7e1af1047f4d4f188d89deb70d708fa9110a
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -74,6 +74,9 @@ SYSCALL_DEFINE3(dup3, unsigned int, oldfd, unsigned int, newfd, int, flags)
 		return -EMFILE;
 	}
 
+	if (newfd >= rlimit(RLIMIT_NOFILE))
+		return -EMFILE;
+
 	spin_lock(&files->file_lock);
 	err = expand_files(files, newfd);
 	file = fcheck(oldfd);
