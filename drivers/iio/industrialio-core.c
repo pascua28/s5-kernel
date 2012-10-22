3d65926ec63c952d3113d75a2e4f8de540742ade
Lars-Peter Clausen <lars@metafoo.de>
2012-10-22 12:15:00 +0100

    iio: Do not accept multiple '.' in fixed point numbers
    Currently when parsing a fix-point number we silently skip any additional '.'
    found in the string.  E.g. '1.2.3.4' gets parsed as '1.234'. This patch
    disallows this and returns an error if more than one '.' is encountered.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -391,7 +391,7 @@ static ssize_t iio_write_channel_info(struct device *dev,
 				break;
 			else
 				return -EINVAL;
-		} else if (*buf == '.') {
+		} else if (*buf == '.' && integer_part) {
 			integer_part = false;
 		} else {
 			return -EINVAL;
