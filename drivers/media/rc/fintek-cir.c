a21d5136e7a43944a8e3d8ad084fe62a0eb38eae
Matthijs Kooijman <matthijs@stdin.nl>
2012-11-02 09:13:55 -0300

    [media] rc: Set rdev before irq setup
    This fixes a problem in fintek-cir and nuvoton-cir where the
    irq handler would trigger during module load before the rdev member was
    set, causing a NULL pointer crash.
    It seems this crash is very reproducible (just bombard the receiver with
    IR signals during module load), probably because when request_irq is
    called, any pending intterupt is handled immediately, before
    request_irq returns and rdev can be set.
    This same crash was supposed to be fixed by commit
    9ef449c6b31bb6a8e6dedc24de475a3b8c79be20 ("[media] rc: Postpone ISR
    registration"), but the crash was still observed on the nuvoton-cir
    driver.
    This commit was tested on nuvoton-cir only.
    
    Cc: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Matthijs Kooijman <matthijs@stdin.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/rc/fintek-cir.c
+++ drivers/media/rc/fintek-cir.c
@@ -548,6 +548,8 @@ static int fintek_probe(struct pnp_dev *pdev, const struct pnp_device_id *dev_id
 	/* rx resolution is hardwired to 50us atm, 1, 25, 100 also possible */
 	rdev->rx_resolution = US_TO_NS(CIR_SAMPLE_PERIOD);
 
+	fintek->rdev = rdev;
+
 	ret = -EBUSY;
 	/* now claim resources */
 	if (!request_region(fintek->cir_addr,
@@ -563,7 +565,7 @@ static int fintek_probe(struct pnp_dev *pdev, const struct pnp_device_id *dev_id
 		goto exit_free_irq;
 
 	device_init_wakeup(&pdev->dev, true);
-	fintek->rdev = rdev;
+
 	fit_pr(KERN_NOTICE, "driver has been successfully loaded\n");
 	if (debug)
 		cir_dump_regs(fintek);
