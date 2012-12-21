646e56ff357ca9cfde372340982cd9438413b7f0
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-12-21 15:16:45 -0800

    misc: remove __dev* attributes.
    CONFIG_HOTPLUG is going away as an option.  As a result, the __dev*
    markings need to be removed.
    
    This change removes the last of the __dev* markings from the kernel from
    a variety of different, tiny, places.
    
    Based on patches originally written by Bill Pemberton, but redone by me
    in order to handle some of the coding style issues better, by hand.
    
    Cc: Bill Pemberton <wfp5p@virginia.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/file.c
+++ fs/file.c
@@ -405,7 +405,7 @@ out:
 	return NULL;
 }
 
-static void __devinit fdtable_defer_list_init(int cpu)
+static void fdtable_defer_list_init(int cpu)
 {
 	struct fdtable_defer *fddef = &per_cpu(fdtable_defer_list, cpu);
 	spin_lock_init(&fddef->lock);
