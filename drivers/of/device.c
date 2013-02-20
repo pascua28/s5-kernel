16929d23712420e25b804a6a78ec2f62f4439ae4
Linus Torvalds <torvalds@linux-foundation.org>
2013-02-20 11:04:46 -0800

    Merge tag 'dt-for-linus' of git://git.secretlab.ca/git/linux
    Pull device tree changes from Grant Likely:
     "All around device tree changes destined for v3.8.  Aside from the
      documentation updates the highlights in this branch include:
    
       - Kbuild changes for using CPP with .dts files
       - locking fix from preempt_rt patchset
       - include DT alias names in device uevent
       - Selftest bugfixes and improvements
       - New function for counting phandles stanzas in a property
       - constify argument to of_node_full_name()
       - Various bug fixes
    
      This tree did also contain a commit to use platform_device_add instead
      of open-coding the device add code, but it caused problems with amba
      devices and needed to be reverted."
    
    * tag 'dt-for-linus' of git://git.secretlab.ca/git/linux: (23 commits)
      Revert "of: use platform_device_add"
      kbuild: limit dtc+cpp include path
      gpio: Make of_count_named_gpios() use new of_count_phandle_with_args()
      of: Create function for counting number of phandles in a property
      of/base: Clean up exit paths for of_parse_phandle_with_args()
      of/selftest: Use selftest() macro throughout
      of/selftest: Fix GPIOs selftest to cover the 7th case
      of: fix recursive locking in of_get_next_available_child()
      documentation/devicetree: Fix a typo in exynos-dw-mshc.txt
      OF: convert devtree lock from rw_lock to raw spinlock
      of/exynos_g2d: Add Bindings for exynos G2D driver
      kbuild: create a rule to run the pre-processor on *.dts files
      input: Extend matrix-keypad device tree binding
      devicetree: Move NS2 LEDs binding into LEDs directory
      of: use platform_device_add
      powerpc/5200: Fix size to request_mem_region() call
      documentation/devicetree: Fix typos
      of: add 'const' to of_node_full_name parameter
      of: Output devicetree alias names in uevent
      DT: add vendor prefixes for Renesas and Toshiba
      ...

--- drivers/of/device.c
+++ drivers/of/device.c
@@ -165,10 +165,6 @@ void of_device_uevent(struct device *dev, struct kobj_uevent_env *env)
 			seen++;
 		}
 	}
-
-	if (seen)
-		add_uevent_var(env, "OF_ALIAS_N=%d", seen);
-
 	mutex_unlock(&of_aliases_mutex);
 }
 
--- drivers/of/of_private.h
+++ drivers/of/of_private.h
@@ -1,21 +1,15 @@
+#ifndef _LINUX_OF_PRIVATE_H
+#define _LINUX_OF_PRIVATE_H
 /*
- * Procedures for creating, accessing and interpreting the device tree.
+ * Private symbols used by OF support code
  *
  * Paul Mackerras	August 1996.
  * Copyright (C) 1996-2005 Paul Mackerras.
  *
- *  Adapted for 64bit PowerPC by Dave Engebretsen and Peter Bergner.
- *    {engebret|bergner}@us.ibm.com
- *
- *  Adapted for sparc and sparc64 by David S. Miller davem@davemloft.net
- *
- *  Reconsolidated from arch/x/kernel/prom.c by Stephen Rothwell and
- *  Grant Likely.
- *
- *      This program is free software; you can redistribute it and/or
- *      modify it under the terms of the GNU General Public License
- *      as published by the Free Software Foundation; either version
- *      2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
  */
 
 /**
@@ -39,3 +33,4 @@ struct alias_prop {
 
 extern struct mutex of_aliases_mutex;
 extern struct list_head aliases_lookup;
+#endif /* _LINUX_OF_PRIVATE_H */
