1e4a8cc5added65b53eb70ac766c02b60a9f88ad
Michal Marek <mmarek@suse.cz>
2013-02-25 13:47:53 +0100

    kbuild: Fix reading of .config in link-vmlinux.sh
    The shell '.' command is not required to search the current directory as
    a fallback and in fact newer versions of bash in sh-mode do not do this.
    Force reading the file from the current directory if $KCONFIG_CONFIG
    contains no '/'.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -127,7 +127,14 @@ if [ "$1" = "clean" ]; then
 fi
 
 # We need access to CONFIG_ symbols
-. ${KCONFIG_CONFIG}
+case "${KCONFIG_CONFIG}" in
+*/*)
+	. "${KCONFIG_CONFIG}"
+	;;
+*)
+	# Force using a file from the current directory
+	. "./${KCONFIG_CONFIG}"
+esac
 
 #link vmlinux.o
 info LD vmlinux.o
