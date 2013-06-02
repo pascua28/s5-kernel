e1a23a3ce4b52551bb10f36d585c3cae82f47394
Stephen Boyd <sboyd@codeaurora.org>
2013-06-01 23:39:40 -0700

    sched_clock: Make ARM's sched_clock generic for all architectures
    Nothing about the sched_clock implementation in the ARM port is
    specific to the architecture. Generalize the code so that other
    architectures can use it by selecting GENERIC_SCHED_CLOCK.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    [jstultz: Merge minor collisions with other patches in my tree]
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Git-commit: 38ff87f77af0b5a93fc8581cff1d6e5692ab8970
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [imaund@codeaurora.org: resolve merge conflicts]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- init/main.c
+++ init/main.c
@@ -74,6 +74,8 @@
 #include <linux/ptrace.h>
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
+#include <linux/random.h>
+#include <linux/sched_clock.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -708,6 +710,7 @@ asmlinkage void __init start_kernel(void)
 	softirq_init();
 	timekeeping_init();
 	time_init();
+	sched_clock_postinit();
 	profile_init();
 	call_function_init();
 	WARN(!irqs_disabled(), "Interrupts were enabled early\n");
