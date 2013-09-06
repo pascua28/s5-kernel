0624063fab8f9184ad592baab7afa358a68f0714
Laura Abbott <lauraa@codeaurora.org>
2013-09-06 14:39:33 -0700

    arm: mm: Account for lowmem in the static vm
    Linux page directory size differs from the ARM section size used
    for lowmem mappings (2MB vs. 1MB). The ioremap code takes this
    into account when static mappings are set up and pads PMD sections
    accordingly. Lowmem needs to be tracked in the static mappings as
    well to account for the size difference as well. If the lowmem
    is not tracked, we may attempt to use part of an address range that
    should be reserved which can trigger BUGs() or unexpected faults.
    
    Change-Id: I60b1ce50a7cafbd189066d7331ee8b4c4ce3027b
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1387,7 +1387,7 @@ static void __init kmap_init(void)
 static void __init map_lowmem(void)
 {
 	struct memblock_region *reg;
-	struct vm_struct *vm;
+	struct static_vm *svm;
 	phys_addr_t start;
 	phys_addr_t end;
 	unsigned long vaddr;
@@ -1428,9 +1428,10 @@ static void __init map_lowmem(void)
 	create_mapping(&map, true);
 #endif
 
-	vm = early_alloc_aligned(sizeof(*vm) * nr, __alignof__(*vm));
+	svm = early_alloc_aligned(sizeof(*svm) * nr, __alignof__(*svm));
 
 	for_each_memblock(memory, reg) {
+		struct vm_struct *vm;
 
 		start = reg->base;
 		end = start + reg->size;
@@ -1440,6 +1441,7 @@ static void __init map_lowmem(void)
 		if (start >= end)
 			break;
 
+		vm = &svm->vm;
 		pfn = __phys_to_pfn(start);
 		vaddr = __phys_to_virt(start);
 		length = end - start;
@@ -1448,10 +1450,10 @@ static void __init map_lowmem(void)
 		vm->addr = (void *)(vaddr & PAGE_MASK);
 		vm->size = PAGE_ALIGN(length + (vaddr & ~PAGE_MASK));
 		vm->phys_addr = __pfn_to_phys(pfn);
-		vm->flags = VM_LOWMEM | VM_ARM_STATIC_MAPPING;
+		vm->flags = VM_LOWMEM;
 		vm->flags |= VM_ARM_MTYPE(type);
 		vm->caller = map_lowmem;
-		vm_area_add_early(vm++);
+		add_static_vm_early(svm++);
 	}
 }
 
