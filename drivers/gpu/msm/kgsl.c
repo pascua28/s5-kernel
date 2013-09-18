721f7f390124da6855a1d33d1ef8d76fb0f7d4e4
Jeremy Gebben <jgebben@codeaurora.org>
2013-09-18 16:03:46 -0600

    msm: kgsl: store process comm name in kgsl_process_private
    Now that contexts keep a reference on their owning
    kgsl_process_private, look up pid and process from there.
    If the process is exiting while hanging the GPU, the
    process name might not be available anymore by the time
    we make the snapshot.
    
    Issue: SEC-3014
    Change-Id: If05ae2096dd7d25bfe9931e949759e11ba3cd262
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>
    (cherry-picked from commit https://source.codeaurora.org/quic/la/kernel/msm-3.10/commit/?id=06ab8cc77d0a8eb32edeadf5c445a643f0df0c1b)

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -581,7 +581,6 @@ int kgsl_context_init(struct kgsl_device_private *dev_priv,
 	context->device = dev_priv->device;
 	context->dev_priv = dev_priv;
 	context->proc_priv = dev_priv->process_priv;
-	context->pid = task_tgid_nr(current);
 	context->tid = task_pid_nr(current);
 
 	ret = kgsl_sync_timeline_create(context);
@@ -1005,6 +1004,8 @@ kgsl_get_process_private(struct kgsl_device *device)
 
 	if (test_bit(KGSL_PROCESS_INIT, &private->priv))
 		goto done;
+
+	get_task_comm(private->comm, current->group_leader);
 
 	private->mem_rb = RB_ROOT;
 	idr_init(&private->mem_idr);
