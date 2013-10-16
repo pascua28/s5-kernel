846ed16da72df7b6be99c4860041428f820adc46
Abhimanyu Kapur <abhimany@codeaurora.org>
2013-10-15 18:15:17 -0700

    sched: Fix compiler warnings
    Gcc-4.8 issues some pointer type cast warnings.
    Fix them.
    
    kernel/kernel/sched/core.c: In function 'try_to_wake_up':
    kernel/kernel/sched/core.c:1548:14: warning: cast to pointer from integer of
    different size [-Wint-to-pointer-cast]
    error, forbidden warning: core.c:1548
    make[4]: *** [kernel/sched/core.o] Error 1
    make[3]: *** [kernel/sched] Error 2
    make[3]: *** Waiting for unfinished jobs....
    
    Change-Id: I1808b435a0e6fdeacac7b8842a80452825205303
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1511,7 +1511,8 @@ static int
 try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 {
 	unsigned long flags;
-	int cpu, src_cpu, success = 0;
+	int cpu, success = 0;
+	unsigned long src_cpu;
 
 	/*
 	 * If we are going to wake up a thread waiting for CONDITION we
@@ -5180,9 +5181,12 @@ done:
 fail:
 	double_rq_unlock(rq_src, rq_dest);
 	raw_spin_unlock(&p->pi_lock);
-	if (moved && task_notify_on_migrate(p))
+	if (moved && task_notify_on_migrate(p)) {
+		unsigned long _src_cpu;
+		_src_cpu = src_cpu;
 		atomic_notifier_call_chain(&migration_notifier_head,
-					   dest_cpu, (void *)src_cpu);
+					   dest_cpu, (void *)_src_cpu);
+	}
 	return ret;
 }
 
