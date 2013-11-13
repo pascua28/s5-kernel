66e7861637c594558f9c8aa7dd8494d76819f18e
Laura Abbott <lauraa@codeaurora.org>
2013-11-13 10:45:25 -0800

    cma: use pfn instead of pages for argument passing
    The CMA code is generic enough that it can be expanded out to track
    regions of memory that aren't officially managed by the regular page
    allocator. This memory can't be referenced via struct page. Change the
    CMA apis to track using pfn for allocation/free instead. The pfn can
    be converted to a struct page as needed elsewhere.
    
    Change-Id: I5ac3fa5e2169b2101a738177f1654faa401f7604
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- include/linux/dma-contiguous.h
+++ include/linux/dma-contiguous.h
@@ -80,9 +80,9 @@ int dma_contiguous_add_device(struct device *dev, phys_addr_t base);
 int dma_declare_contiguous(struct device *dev, phys_addr_t size,
 			   phys_addr_t base, phys_addr_t limit);
 
-struct page *dma_alloc_from_contiguous(struct device *dev, int count,
+unsigned long dma_alloc_from_contiguous(struct device *dev, int count,
 				       unsigned int order);
-bool dma_release_from_contiguous(struct device *dev, struct page *pages,
+bool dma_release_from_contiguous(struct device *dev, unsigned long pfn,
 				 int count);
 
 #else
@@ -99,14 +99,14 @@ int dma_declare_contiguous(struct device *dev, phys_addr_t size,
 }
 
 static inline
-struct page *dma_alloc_from_contiguous(struct device *dev, int count,
+unsigned long dma_alloc_from_contiguous(struct device *dev, int count,
 				       unsigned int order)
 {
-	return NULL;
+	return 0;
 }
 
 static inline
-bool dma_release_from_contiguous(struct device *dev, struct page *pages,
+bool dma_release_from_contiguous(struct device *dev, unsigned long pfn,
 				 int count)
 {
 	return false;
