0b178199e3a75a3fd1bc09175d711bc5c58056ab
Saravana Kannan <skannan@codeaurora.org>
2014-02-24 16:22:15 -0800

    cpufreq: stats: Fix error handling in __cpufreq_stats_create_table()
    Remove sysfs group if __cpufreq_stats_create_table() fails after creating
    one.
    
    Change-Id: Icb0b44424cc4eb6c88be255e2839ef51c3f8779c
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -223,7 +223,7 @@ static int __cpufreq_stats_create_table(struct cpufreq_policy *policy,
 	stat->time_in_state = kzalloc(alloc_size, GFP_KERNEL);
 	if (!stat->time_in_state) {
 		ret = -ENOMEM;
-		goto error_out;
+		goto error_alloc;
 	}
 	stat->freq_table = (unsigned int *)(stat->time_in_state + count);
 
@@ -245,6 +245,8 @@ static int __cpufreq_stats_create_table(struct cpufreq_policy *policy,
 	spin_unlock(&cpufreq_stats_lock);
 	cpufreq_cpu_put(current_policy);
 	return 0;
+error_alloc:
+	sysfs_remove_group(&policy->kobj, &stats_attr_group);
 error_out:
 	cpufreq_cpu_put(current_policy);
 error_get_fail:
