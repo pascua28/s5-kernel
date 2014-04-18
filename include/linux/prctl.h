4f86f14ec4484d3646c78bdba81fb9199a952aaf
Ruchi Kandoi <kandoiruchi@google.com>
2014-04-18 14:07:28 -0700

    prctl: adds PR_SET_TIMERSLACK_PID for setting timer slack of an arbitrary thread.
    Second argument is similar to PR_SET_TIMERSLACK, if non-zero then the
    slack is set to that value otherwise sets it to the default for the thread.
    
    Takes PID of the thread as the third argument.
    
    This allows power/performance management software to set timer slack for
    other threads according to its policy for the thread (such as when the
    thread is designated foreground vs. background activity)
    
    Change-Id: I744d451ff4e60dae69f38f53948ff36c51c14a3f
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Git-commit: 011ca51c345eeae6838c929bb2114a5541b47039
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- include/linux/prctl.h
+++ include/linux/prctl.h
@@ -132,6 +132,12 @@
  #define PR_SET_TIMERSLACK_PID 41
 #endif
 
+/* Sets the timerslack for arbitrary threads
+ * arg2 slack value, 0 means "use default"
+ * arg3 pid of the thread whose timer slack needs to be set
+ */
+#define PR_SET_TIMERSLACK_PID 41
+
 #define PR_SET_VMA		0x53564d41
 # define PR_SET_VMA_ANON_NAME		0
 
