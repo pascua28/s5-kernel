88748ecddbf506da7702df7b08b0148ffb836599
Andrew Dodd <atd7@cornell.edu>
2014-04-19 14:57:11 -0400

    Find 7 changes applied to AU_LINUX_ANDROID_KK_3.5.04.04.02.003.298: drivers/media/platform (camera)
    minus msm_sensor.c because it needs major merge work
    minus drivers/media/platform/msm/vidc/venus_hfi.c because it also needs major merge work
    
    From LNX.LA.3.2.5-00210-8x74.0
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/msm_buf_mgr/msm_generic_buf_mgr.c
    	drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
    	drivers/media/platform/msm/camera_v2/sensor/Makefile
    	drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
    	drivers/media/platform/msm/vidc/venus_hfi.c

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1159,7 +1159,8 @@ int msm_isp_close_node(struct v4l2_subdev *sd, struct v4l2_subdev_fh *fh)
 
 	rc = vfe_dev->hw_info->vfe_ops.axi_ops.halt(vfe_dev);
 	if (rc <= 0)
-		pr_err("%s: halt timeout rc=%ld\n", __func__, rc);
+		pr_err("%s: halt timeout rc=%ld id %d\n", __func__, rc,
+			vfe_dev->pdev->id);
 
 	vfe_dev->buf_mgr->ops->buf_mgr_deinit(vfe_dev->buf_mgr);
 	vfe_dev->hw_info->vfe_ops.core_ops.release_hw(vfe_dev);
