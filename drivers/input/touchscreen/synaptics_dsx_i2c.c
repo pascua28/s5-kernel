64d52e9f978c6d94e91dcbeb9192c6b0691ba974
Andrew Dodd <atd7@cornell.edu>
2014-05-24 11:05:46 -0400

    synaptics: Disable gesture v2.0
    This causes the gesture system to return KEY_F4 and a series of points
    regardless of actual gesture drawn.
    
    Change-Id: Ifa7981df63a6e675d0ca64ec40d2ef186a5807e5

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -1003,7 +1003,7 @@ static struct synaptics_rmi4_data *syna_rmi4_data=0;
 static struct regulator *vdd_regulator=0;
 static struct regulator *vdd_regulator_i2c=0;
 static int syna_use_gesture = 0 ;
-static int syna_use_gesture2 = 1 ;  //flag to support tp2.0
+static int syna_use_gesture2 = 0 ;  //flag to support tp2.0
 static int syna_test_max_err_count = 10;
 static char synaptics_vendor_str[32];  //vendor string
 #ifdef CONFIG_OPPO_DEVICE_INFO
