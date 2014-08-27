441c15c1f08265c3bd57dd9d6d557c92ec6b0efc
Mathias Krause <minipli@googlemail.com>
2014-08-27 20:28:56 +0930

    modpost: simplify file name generation of *.mod.c files
    Avoid the variable length array (vla), just use PATH_MAX instead.
    This not only makes this code clang friedly, it also leads to a
    code size reduction:
    
       text    data     bss     dec     hex filename
      51765    2224   12416   66405   10365 scripts/mod/modpost.old
      51677    2224   12416   66317   1030d scripts/mod/modpost.new
    
    Signed-off-by: Mathias Krause <minipli@googlemail.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -2217,7 +2217,7 @@ int main(int argc, char **argv)
 	err = 0;
 
 	for (mod = modules; mod; mod = mod->next) {
-		char fname[strlen(mod->name) + 10];
+		char fname[PATH_MAX];
 
 		if (mod->skip)
 			continue;
