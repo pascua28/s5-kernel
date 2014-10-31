597ff3c6afb46eb49aca1868bc66daafac1ce798
Hemant Kumar <hemantk@codeaurora.org>
2014-10-31 14:04:04 -0700

    usb: dwc3: Do not call WARN_ON_ONCE from interrupt context
    Gadget driver calls WARN_ON_ONCE in some of the APIs which
    are called from interrupt context. This may lead to watchdog
    bark if repeated WARN_ON_ONCE condition occurs. Hence print
    error message and/or log it in debug buffer.
    
    CRs-Fixed: 747698
    Change-Id: Ieecd6342135e7d2bad36c82a12958b78054150db
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -959,7 +959,6 @@ static void __dwc3_ep0_do_control_data(struct dwc3 *dwc,
 	}
 
 	dbg_queue(dep->number, &req->request, ret);
-	WARN_ON(ret < 0);
 }
 
 static int dwc3_ep0_start_control_status(struct dwc3_ep *dep)
@@ -1010,7 +1009,11 @@ static void dwc3_ep0_end_control_data(struct dwc3 *dwc, struct dwc3_ep *dep)
 	cmd |= DWC3_DEPCMD_PARAM(dep->resource_index);
 	memset(&params, 0, sizeof(params));
 	ret = dwc3_send_gadget_ep_cmd(dwc, dep->number, cmd, &params);
-	WARN_ON_ONCE(ret);
+	if (ret) {
+		dev_dbg(dwc->dev, "%s: send ep cmd ENDTRANSFER failed",
+			dep->name);
+		dbg_event(dep->number, "EENDXFER", ret);
+	}
 	dep->resource_index = 0;
 }
 
@@ -1019,6 +1022,7 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 {
 	u8			epnum;
 	int			ret;
+	struct dwc3_ep	*dep;
 
 	dwc->setup_packet_pending = true;
 	epnum = event->endpoint_number;
@@ -1036,8 +1040,8 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 		 * Phase we already have started and issue SetStall on the
 		 * control endpoint.
 		 */
+		dep = dwc->eps[dwc->ep0_expect_in];
 		if (dwc->ep0_expect_in != event->endpoint_number) {
-			struct dwc3_ep	*dep = dwc->eps[dwc->ep0_expect_in];
 
 			dev_vdbg(dwc->dev, "Wrong direction for Data phase\n");
 			dwc3_ep0_end_control_data(dwc, dep);
@@ -1052,7 +1056,9 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 					dwc->ctrl_req_addr, 0,
 					DWC3_TRBCTL_CONTROL_DATA);
 			dbg_event(epnum, "ZLP", ret);
-			WARN_ON(ret < 0);
+			if (ret)
+				dev_dbg(dwc->dev, "%s: start xfer cmd failed",
+					dep->name);
 		}
 
 		break;
@@ -1068,7 +1074,8 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 
 		if (dwc->delayed_status &&
 				list_empty(&dwc->eps[0]->request_list)) {
-			WARN_ON_ONCE(event->endpoint_number != 1);
+			if (event->endpoint_number != 1)
+				dbg_event(epnum, "EEPNUM", event->status);
 			dev_vdbg(dwc->dev, "Mass Storage delayed status\n");
 			return;
 		}
