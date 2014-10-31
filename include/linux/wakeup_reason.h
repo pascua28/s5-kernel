e88e62fc00d778334d9fc1189cc3190a8613951a
Dmitry Shmidt <dimitrysh@google.com>
2014-10-31 16:05:46 -0700

    power: Add check_wakeup_reason() to verify wakeup source irq
    Wakeup reason is set before driver resume handlers are called.
    It is cleared before driver suspend handlers are called, on
    PM_SUSPEND_PREPARE.
    
    Change-Id: I04218c9b0c115a7877e8029c73e6679ff82e0aa4
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wakeup_reason.h
+++ include/linux/wakeup_reason.h
@@ -22,6 +22,7 @@
 
 void log_wakeup_reason(int irq);
 void log_suspend_abort_reason(const char *fmt, ...);
+int check_wakeup_reason(int irq);
 #ifdef CONFIG_ARCH_MSM
 void update_wakeup_reason_stats(int irq);
 #endif
