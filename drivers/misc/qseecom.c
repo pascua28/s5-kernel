2ae5f34c617f801428c744862a4a39daae63befa
Mona Hossain <mhossain@codeaurora.org>
2014-11-03 17:05:48 -0800

    qseecom: Add checks for send_cmd inputs
    Improve user input validation across send cmd APIs. Add new
    API  __validate_send_cmd_inputs() to validate all user provided
    inputs.
    
    Signed-off-by: Mona Hossain <mhossain@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1490,6 +1490,7 @@ exit:
 
 static int __validate_send_cmd_inputs(struct qseecom_dev_handle *data,
 				struct qseecom_send_cmd_req *req)
+
 {
 	if (!data || !data->client.ihandle) {
 		pr_err("Client or client handle is not initialized\n");
@@ -1506,10 +1507,10 @@ static int __validate_send_cmd_inputs(struct qseecom_dev_handle *data,
 		pr_err("cmd buffer address not within shared bufffer\n");
 		return -EINVAL;
 	}
-	if (((uintptr_t)req->resp_buf <
-				data->client.user_virt_sb_base)  ||
-		((uintptr_t)req->resp_buf >=
-		(data->client.user_virt_sb_base + data->client.sb_length))) {
+
+	if (((uintptr_t)req->resp_buf < data->client.user_virt_sb_base)  ||
+		((uintptr_t)req->resp_buf >= (data->client.user_virt_sb_base +
+					data->client.sb_length))) {
 		pr_err("response buffer address not within shared bufffer\n");
 		return -EINVAL;
 	}
@@ -1525,8 +1526,8 @@ static int __validate_send_cmd_inputs(struct qseecom_dev_handle *data,
 	}
 
 	if ((req->cmd_req_len + req->resp_len) > data->client.sb_length) {
-		pr_debug("Not enough memory to fit cmd_buf.\n");
-		pr_debug("resp_buf. Required: %u, Available: %zu\n",
+		pr_debug("Not enough memory to fit cmd_buf and "
+			"resp_buf. Required: %u, Available: %u\n",
 				(req->cmd_req_len + req->resp_len),
 					data->client.sb_length);
 		return -ENOMEM;
@@ -1841,7 +1842,7 @@ static int __qseecom_update_cmd_buf(void *msg, bool cleanup,
 				}
 			} else if ((data->type == QSEECOM_LISTENER_SERVICE) &&
 					(lstnr_resp->ifd_data[i].fd > 0)) {
-				if ((lstnr_resp->resp_len <
+					if ((lstnr_resp->resp_len <
 					SG_ENTRY_SZ * sg_ptr->nents) ||
 					(lstnr_resp->ifd_data[i].cmd_buf_offset >
 					(lstnr_resp->resp_len -
