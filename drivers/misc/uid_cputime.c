1e342166ff5d08931671517ada4cdc444df08eb3
jinqian <jinqian@google.com>
2015-03-11 10:44:50 -0700

    proc: uid: Adds accounting for the cputimes per uid.
    Adds proc files /proc/uid_cputime/show_uid_stat and
    /proc/uid_cputime/remove_uid_range.
    
    show_uid_stat lists the total utime and stime for the active as well as
    terminated processes for each of the uids.
    
    Writing a range of uids to remove_uid_range will delete the accounting
    for all the uids within that range.
    
    Change-Id: I21d9210379da730b33ddc1a0ea663c8c9d2ac15b

--- drivers/misc/uid_cputime.c
+++ drivers/misc/uid_cputime.c
@@ -38,16 +38,13 @@ struct uid_entry {
 	cputime_t stime;
 	cputime_t active_utime;
 	cputime_t active_stime;
-	unsigned long long active_power;
-	unsigned long long power;
 	struct hlist_node hash;
 };
 
 static struct uid_entry *find_uid_entry(uid_t uid)
 {
 	struct uid_entry *uid_entry;
-	struct hlist_node *node;
-	hash_for_each_possible(hash_table, uid_entry, node, hash, uid) {
+	hash_for_each_possible(hash_table, uid_entry, hash, uid) {
 		if (uid_entry->uid == uid)
 			return uid_entry;
 	}
@@ -76,20 +73,20 @@ static struct uid_entry *find_or_register_uid(uid_t uid)
 static int uid_stat_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
-	struct task_struct *task, *temp;
+	struct task_struct *task;
+	cputime_t utime;
+	cputime_t stime;
 	unsigned long bkt;
-	struct hlist_node *node;
 
 	mutex_lock(&uid_lock);
 
-	hash_for_each(hash_table, bkt, node, uid_entry, hash) {
+	hash_for_each(hash_table, bkt, uid_entry, hash) {
 		uid_entry->active_stime = 0;
 		uid_entry->active_utime = 0;
-		uid_entry->active_power = 0;
 	}
 
 	read_lock(&tasklist_lock);
-	do_each_thread(temp, task) {
+	for_each_process(task) {
 		uid_entry = find_or_register_uid(task_uid(task));
 		if (!uid_entry) {
 			read_unlock(&tasklist_lock);
@@ -98,29 +95,20 @@ static int uid_stat_show(struct seq_file *m, void *v)
 						__func__, task_uid(task));
 			return -ENOMEM;
 		}
-		/* if this task is exiting, we have already accounted for the
-		 * time and power. */
-		if (task->cpu_power == ULLONG_MAX)
-			continue;
-		uid_entry->active_utime += task->utime;
-		uid_entry->active_stime += task->stime;
-		uid_entry->active_power += task->cpu_power;
-	} while_each_thread(temp, task);
+		task_cputime_adjusted(task, &utime, &stime);
+		uid_entry->active_utime += utime;
+		uid_entry->active_stime += stime;
+	}
 	read_unlock(&tasklist_lock);
 
-	hash_for_each(hash_table, bkt, node, uid_entry, hash) {
+	hash_for_each(hash_table, bkt, uid_entry, hash) {
 		cputime_t total_utime = uid_entry->utime +
 							uid_entry->active_utime;
 		cputime_t total_stime = uid_entry->stime +
 							uid_entry->active_stime;
-		unsigned long long total_power = uid_entry->power +
-							uid_entry->active_power;
-		seq_printf(m, "%d: %llu %llu %llu\n", uid_entry->uid,
-			(unsigned long long)jiffies_to_msecs(
-				cputime_to_jiffies(total_utime)) * USEC_PER_MSEC,
-			(unsigned long long)jiffies_to_msecs(
-				cputime_to_jiffies(total_stime)) * USEC_PER_MSEC,
-			total_power);
+		seq_printf(m, "%d: %u %u\n", uid_entry->uid,
+						cputime_to_usecs(total_utime),
+						cputime_to_usecs(total_stime));
 	}
 
 	mutex_unlock(&uid_lock);
@@ -129,7 +117,7 @@ static int uid_stat_show(struct seq_file *m, void *v)
 
 static int uid_stat_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, uid_stat_show, PDE(inode)->data);
+	return single_open(file, uid_stat_show, PDE_DATA(inode));
 }
 
 static const struct file_operations uid_stat_fops = {
@@ -152,7 +140,6 @@ static ssize_t uid_remove_write(struct file *file,
 	char uids[128];
 	char *start_uid, *end_uid = NULL;
 	long int uid_start = 0, uid_end = 0;
-	struct hlist_node *node;
 
 	if (count >= sizeof(uids))
 		count = sizeof(uids) - 1;
@@ -175,8 +162,8 @@ static ssize_t uid_remove_write(struct file *file,
 	mutex_lock(&uid_lock);
 
 	for (; uid_start <= uid_end; uid_start++) {
-		hash_for_each_possible_safe(hash_table, uid_entry, node,
-						   tmp, hash, uid_start) {
+		hash_for_each_possible_safe(hash_table, uid_entry, tmp,
+							hash, uid_start) {
 			hash_del(&uid_entry->hash);
 			kfree(uid_entry);
 		}
@@ -197,6 +184,7 @@ static int process_notifier(struct notifier_block *self,
 {
 	struct task_struct *task = v;
 	struct uid_entry *uid_entry;
+	cputime_t utime, stime;
 	uid_t uid;
 
 	if (!task)
@@ -210,10 +198,9 @@ static int process_notifier(struct notifier_block *self,
 		goto exit;
 	}
 
-	uid_entry->utime += task->utime;
-	uid_entry->stime += task->stime;
-	uid_entry->power += task->cpu_power;
-	task->cpu_power = ULLONG_MAX;
+	task_cputime_adjusted(task, &utime, &stime);
+	uid_entry->utime += utime;
+	uid_entry->stime += stime;
 
 exit:
 	mutex_unlock(&uid_lock);
@@ -237,7 +224,7 @@ static int __init proc_uid_cputime_init(void)
 	proc_create_data("remove_uid_range", S_IWUGO, parent, &uid_remove_fops,
 					NULL);
 
-	proc_create_data("show_uid_stat", S_IRUGO, parent, &uid_stat_fops,
+	proc_create_data("show_uid_stat", S_IWUGO, parent, &uid_stat_fops,
 					NULL);
 
 	profile_event_register(PROFILE_TASK_EXIT, &process_notifier_block);
