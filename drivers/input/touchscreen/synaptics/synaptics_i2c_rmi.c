59a36beb0036dcd3a25dec56ddd1a3b8485ad1b3
andip71 <andreasp@gmx.de>
2015-06-19 11:25:47 +0200

    ssp_i2c.c: Implement missing on-call/no-call hooks for wake gestures
    - this fixes issues with proximity sensor not working during a call

--- drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
@@ -5812,7 +5812,6 @@ static int synaptics_rmi4_stop_device(struct synaptics_rmi4_data *rmi4_data)
 	if (wake_options_okto_enable())
 	{
 		enable_irq_wake(rmi4_data->i2c_client->irq);
-		wake_funcs_set_prox(true);
 		screen_wake_options_when_off = screen_wake_options;
 	}
 	else
@@ -5876,7 +5875,6 @@ static int synaptics_rmi4_start_device(struct synaptics_rmi4_data *rmi4_data)
 	if (screen_wake_options_when_off)
 	{
 		disable_irq_wake(rmi4_data->i2c_client->irq);
-		wake_funcs_set_prox(false);
 	}
 	else
 		enable_irq(rmi4_data->i2c_client->irq);
