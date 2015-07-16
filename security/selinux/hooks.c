3aa9c74af6c4da0d56cbe6b253099eed9643f003
ShinySide <shinyside0524@gmail.com>
2015-07-16 13:31:40 -0400

    security: SELinux: revert Samsung hard enforcing
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -109,11 +109,8 @@ static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
-#ifdef CONFIG_ALWAYS_ENFORCE
-		selinux_enforcing = 1;
-#else
+
 		selinux_enforcing = enforcing ? 1 : 0;
-#endif
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
@@ -126,11 +123,9 @@ static int __init selinux_enabled_setup(char *str)
 {
 	unsigned long enabled;
 	if (!strict_strtoul(str, 0, &enabled))
-#ifdef CONFIG_ALWAYS_ENFORCE
-		selinux_enabled = 1;
-#else
+
 		selinux_enabled = enabled ? 1 : 0;
-#endif
+
 	return 1;
 }
 __setup("selinux=", selinux_enabled_setup);
@@ -4783,11 +4778,7 @@ static int selinux_nlmsg_perm(struct sock *sk, struct sk_buff *skb)
 				sk->sk_protocol, nlh->nlmsg_type,
 				secclass_map[sclass - 1].name,
 				task_pid_nr(current), current->comm);
-#ifdef CONFIG_ALWAYS_ENFORCE
-			if (security_get_allow_unknown())
-#else
 			if (selinux_enforcing && !security_get_allow_unknown())
-#endif
 				return rc;
 			rc = 0;
 		} else if (rc == -ENOENT) {
@@ -6105,9 +6096,7 @@ static __init int selinux_init(void)
 
 	if (register_security(&selinux_ops))
 		panic("SELinux: Unable to register with kernel.\n");
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
+
 	if (selinux_enforcing)
 		printk(KERN_DEBUG "SELinux:  Starting in enforcing mode\n");
 	else
@@ -6184,9 +6173,7 @@ static struct nf_hook_ops selinux_ipv6_ops[] = {
 static int __init selinux_nf_ip_init(void)
 {
 	int err = 0;
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enabled = 1;
-#endif
+
 	if (!selinux_enabled)
 		goto out;
 
