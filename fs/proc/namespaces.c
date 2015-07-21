1972ddb577e92bb308af9c590c150b0e5ec82b7a
Daniel Rosenberg <drosen@google.com>
2015-07-21 15:17:26 -0700

    Fix incorrect conflict resolution in "vfs: Add setns support for the mount namespace"
    Change-Id: I554f28a1f92267ea3681d519c8402d983e799186
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- fs/proc/namespaces.c
+++ fs/proc/namespaces.c
@@ -24,6 +24,7 @@ static const struct proc_ns_operations *ns_entries[] = {
 #ifdef CONFIG_IPC_NS
 	&ipcns_operations,
 #endif
+	&mntns_operations,
 };
 
 static const struct file_operations ns_file_operations = {
