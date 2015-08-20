1272ce471373a3488f65cff43173027832ed197f
Dorimanx <yuri@bynet.co.il>
2015-08-20 15:02:55 +0300

    SMP & GIC: merged updates from 3.10.y manually. + cleanups in boards.
    Signed-off-by: Tomoms <fondatommaso2@gmail.com>

--- arch/arm/include/asm/smp.h
+++ arch/arm/include/asm/smp.h
@@ -92,6 +92,7 @@ extern void platform_cpu_enable(unsigned int cpu);
 
 extern void arch_send_call_function_single_ipi(int cpu);
 extern void arch_send_call_function_ipi_mask(const struct cpumask *mask);
+extern void arch_send_wakeup_ipi_mask(const struct cpumask *mask);
 
 extern void smp_send_all_cpu_backtrace(void);
 
