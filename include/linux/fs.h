e86377e68aead4fd0af15a825eac8b8fbd85d649
eray orçunus <erayorcunus@gmail.com>
2016-04-23 15:51:43 +0300

    Revert "Revert "cache the value of file_inode() in struct file""
    This reverts commit 69d5c3cc001b40f8b36fd0d5f376f48d6b5b1f1f.
    
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -832,6 +832,7 @@ struct file {
 	} f_u;
 	struct path		f_path;
 #define f_dentry	f_path.dentry
+	struct inode		*f_inode;	/* cached value */
 	const struct file_operations	*f_op;
 
 	/*
@@ -2272,7 +2273,7 @@ static inline bool execute_ok(struct inode *inode)
 
 static inline struct inode *file_inode(struct file *f)
 {
-	return f->f_path.dentry->d_inode;
+	return f->f_inode;
 }
 
 static inline void file_start_write(struct file *file)
