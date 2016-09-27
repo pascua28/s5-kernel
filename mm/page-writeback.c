59eba557e4437038d39c8fc8c66256ba126acb84
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2016-09-27 06:04:25 -0700

    page-writeback: Increase these numbers to help save some battery
    Change-Id: I27dc2275b59a211c72428fd1062d23cf0ae1ee00
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>
    
    # Conflicts:
    #	mm/page-writeback.c

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -68,7 +68,7 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 25;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -85,7 +85,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-int vm_dirty_ratio = 20;
+int vm_dirty_ratio = 50;
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
