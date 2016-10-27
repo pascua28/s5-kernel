f02943fcf0998f2793278d48449b0a3c715e861d
mostafa-z <mostafazarghami@gmail.com>
2016-10-28 02:08:45 +0330

    workqueue: cleanup old updates

--- include/linux/workqueue.h
+++ include/linux/workqueue.h
@@ -319,12 +319,6 @@ enum {
  *
  * system_nrt_freezable_wq is equivalent to system_nrt_wq except that
  * it's freezable.
- *
- * *_power_efficient_wq are inclined towards saving power and converted
- * into WQ_UNBOUND variants if 'wq_power_efficient' is enabled; otherwise,
- * they are same as their non-power-efficient counterparts - e.g.
- * system_power_efficient_wq is identical to system_wq if
- * 'wq_power_efficient' is disabled.  See WQ_POWER_EFFICIENT for more info.
  */
 extern struct workqueue_struct *system_wq;
 extern struct workqueue_struct *system_long_wq;
@@ -332,8 +326,6 @@ extern struct workqueue_struct *system_nrt_wq;
 extern struct workqueue_struct *system_unbound_wq;
 extern struct workqueue_struct *system_freezable_wq;
 extern struct workqueue_struct *system_nrt_freezable_wq;
-extern struct workqueue_struct *system_power_efficient_wq;
-extern struct workqueue_struct *system_freezable_power_efficient_wq;
 
 extern struct workqueue_struct *
 __alloc_workqueue_key(const char *fmt, unsigned int flags, int max_active,
@@ -483,3 +475,4 @@ extern void thaw_workqueues(void);
 #endif /* CONFIG_FREEZER */
 
 #endif
+
