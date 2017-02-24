9c81e53edaf55f7202903588bcef258d26d0c5df
Manu Gautam <mgautam@codeaurora.org>
2017-02-24 15:22:40 +0530

    usb: replace %p with %pK
    Format specifier %p can leak kernel addresses while not valuing the
    kptr_restrict system settings. When kptr_restrict is set to (1), kernel
    pointers printed using the %pK format specifier will be replaced with 0's.
    Debugging Note : &pK prints only Zeros as address. If you need actual
    address information, write 0 to kptr_restrict.
    echo 0 > /proc/sys/kernel/kptr_restrict
    
    CRs-fixed: 1052849
    Change-Id: I0e98145730380ea983fa8f46a28d15dd6c2c31df
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    Signed-off-by: Yasir Malik <ymalik@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -220,7 +220,7 @@ int usb_add_function(struct usb_configuration *config,
 {
 	int	value = -EINVAL;
 
-	DBG(config->cdev, "adding '%s'/%p to config '%s'/%p\n",
+	DBG(config->cdev, "adding '%s'/%pK to config '%s'/%pK\n",
 			function->name, function,
 			config->label, config);
 
@@ -254,7 +254,7 @@ int usb_add_function(struct usb_configuration *config,
 
 done:
 	if (value)
-		DBG(config->cdev, "adding '%s'/%p --> %d\n",
+		DBG(config->cdev, "adding '%s'/%pK --> %d\n",
 				function->name, function, value);
 	return value;
 }
@@ -741,7 +741,7 @@ static int set_config(struct usb_composite_dev *cdev,
 
 		result = f->set_alt(f, tmp, 0);
 		if (result < 0) {
-			DBG(cdev, "interface %d (%s/%p) alt 0 --> %d\n",
+			DBG(cdev, "interface %d (%s/%pK) alt 0 --> %d\n",
 					tmp, f->name, f, result);
 
 			reset_config(cdev);
@@ -790,7 +790,7 @@ int usb_add_config(struct usb_composite_dev *cdev,
 	int				status = -EINVAL;
 	struct usb_configuration	*c;
 
-	DBG(cdev, "adding config #%u '%s'/%p\n",
+	DBG(cdev, "adding config #%u '%s'/%pK\n",
 			config->bConfigurationValue,
 			config->label, config);
 
@@ -819,7 +819,7 @@ int usb_add_config(struct usb_composite_dev *cdev,
 	} else {
 		unsigned	i;
 
-		DBG(cdev, "cfg %d/%p speeds:%s%s%s\n",
+		DBG(cdev, "cfg %d/%pK speeds:%s%s%s\n",
 			config->bConfigurationValue, config,
 			config->superspeed ? " super" : "",
 			config->highspeed ? " high" : "",
@@ -834,7 +834,7 @@ int usb_add_config(struct usb_composite_dev *cdev,
 
 			if (!f)
 				continue;
-			DBG(cdev, "  interface %d = %s/%p\n",
+			DBG(cdev, "  interface %d = %s/%pK\n",
 				i, f->name, f);
 		}
 	}
@@ -861,13 +861,13 @@ static int unbind_config(struct usb_composite_dev *cdev,
 				struct usb_function, list);
 		list_del(&f->list);
 		if (f->unbind) {
-			DBG(cdev, "unbind function '%s'/%p\n", f->name, f);
+			DBG(cdev, "unbind function '%s'/%pK\n", f->name, f);
 			f->unbind(config, f);
 			/* may free memory for "f" */
 		}
 	}
 	if (config->unbind) {
-		DBG(cdev, "unbind config '%s'/%p\n", config->label, config);
+		DBG(cdev, "unbind config '%s'/%pK\n", config->label, config);
 		config->unbind(config);
 			/* may free memory for "c" */
 	}
@@ -888,7 +888,7 @@ int usb_remove_config(struct usb_composite_dev *cdev,
 {
 	unsigned long flags;
 
-	printk(KERN_DEBUG "usb: %s cdev->config=%p, config=%p\n",
+	printk(KERN_DEBUG "usb: %s cdev->config=%pK, config=%pK\n",
 			__func__, cdev->config, config);
 	spin_lock_irqsave(&cdev->lock, flags);
 
--- drivers/usb/serial/whiteheat.c
+++ drivers/usb/serial/whiteheat.c
@@ -272,7 +272,7 @@ static int whiteheat_firmware_download(struct usb_serial *serial,
 					      be16_to_cpu(record->len), 0xa0);
 		if (response < 0) {
 			dev_err(&serial->dev->dev, "%s - ezusb_writememory "
-				"failed for loader (%d %04X %p %d)\n",
+				"failed for loader (%d %04X %pK %d)\n",
 				__func__, response, be32_to_cpu(record->addr),
 				record->data, be16_to_cpu(record->len));
 			break;
@@ -292,7 +292,7 @@ static int whiteheat_firmware_download(struct usb_serial *serial,
 		if (response < 0) {
 			dev_err(&serial->dev->dev, "%s - ezusb_writememory "
 				"failed for first firmware step "
-				"(%d %04X %p %d)\n", __func__, response,
+				"(%d %04X %pK %d)\n", __func__, response,
 				be32_to_cpu(record->addr), record->data,
 				be16_to_cpu(record->len));
 			break;
@@ -310,7 +310,7 @@ static int whiteheat_firmware_download(struct usb_serial *serial,
 		if (response < 0) {
 			dev_err(&serial->dev->dev, "%s - ezusb_writememory "
 				"failed for second firmware step "
-				"(%d %04X %p %d)\n", __func__, response,
+				"(%d %04X %pK %d)\n", __func__, response,
 				be32_to_cpu(record->addr), record->data,
 				be16_to_cpu(record->len));
 			break;
