0ea53c1864b1d9f7c69e99ef4cd9ff264c0242cb
eray orçunus <erayorcunus@gmail.com>
2017-02-28 16:00:09 +0300

    sched/core: Kill sched_class::task_waking to clean up the migration l…
    …ogic
    
    With sched_class::task_waking being called only when we do
    set_task_cpu(), we can make sched_class::migrate_task_rq() do the work
    and eliminate sched_class::task_waking entirely.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Andrew Hunter <ahh@google.com>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Mike Galbraith <umgwanakikbuti@gmail.com>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Pavan Kondeti <pkondeti@codeaurora.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: byungchul.park@lge.com
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Ported to MSM 3.10.50 kernel by erorcun <erayorcunus@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -998,9 +998,9 @@ static const u32 prio_to_wmult[40] = {
 #define ENQUEUE_WAKEUP		1
 #define ENQUEUE_HEAD		2
 #ifdef CONFIG_SMP
-#define ENQUEUE_WAKING		4	/* sched_class::task_waking was called */
+#define ENQUEUE_MIGRATED		4	/* the task was migrated during wakeup */
 #else
-#define ENQUEUE_WAKING		0
+#define ENQUEUE_MIGRATED		0
 #endif
 #define ENQUEUE_MIGRATING	8
 
@@ -1026,7 +1026,6 @@ struct sched_class {
 
 	void (*pre_schedule) (struct rq *this_rq, struct task_struct *task);
 	void (*post_schedule) (struct rq *this_rq);
-	void (*task_waking) (struct task_struct *task);
 	void (*task_woken) (struct rq *this_rq, struct task_struct *task);
 
 	void (*set_cpus_allowed)(struct task_struct *p,
