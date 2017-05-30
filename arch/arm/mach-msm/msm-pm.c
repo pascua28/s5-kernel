c100120a8cce3db17c23e3bccecedec6b7ad3fb5
Evan Anderson <evananderson@aospa.co>
2017-05-30 02:10:29 -0500

    ARM: mach-msm: Fix numerous section mismatches
    * Qualcomm writes great code /s
    
    Change-Id: Ibfc1cf5196c9ee33e2f06b689d22976eb1109868
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- arch/arm/mach-msm/msm-pm.c
+++ arch/arm/mach-msm/msm-pm.c
@@ -1028,7 +1028,7 @@ static struct of_device_id msm_slp_sts_match_tbl[] = {
 	{},
 };
 
-static struct platform_driver msm_cpu_status_driver = {
+static struct platform_driver msm_cpu_status_driver __refdata = {
 	.probe = msm_cpu_status_probe,
 	.driver = {
 		.name = "cpu_slp_status",
@@ -1042,7 +1042,7 @@ static struct of_device_id msm_snoc_clnt_match_tbl[] = {
 	{},
 };
 
-static struct platform_driver msm_cpu_pm_snoc_client_driver = {
+static struct platform_driver msm_cpu_pm_snoc_client_driver __refdata = {
 	.probe = msm_pm_snoc_client_probe,
 	.driver = {
 		.name = "pm_snoc_client",
@@ -1309,7 +1309,7 @@ static struct of_device_id msm_cpu_pm_table[] = {
 	{},
 };
 
-static struct platform_driver msm_cpu_pm_driver = {
+static struct platform_driver msm_cpu_pm_driver __refdata = {
 	.probe = msm_cpu_pm_probe,
 	.driver = {
 		.name = "pm-8x60",
