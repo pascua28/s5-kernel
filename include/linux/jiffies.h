e8034735b6d37c7e10a9ba3b9ffe28dbd608c451
Matthias Kaehlcke <mka@chromium.org>
2017-06-02 14:46:16 -0700

    UPSTREAM: frv: declare jiffies to be located in the .data section
    Commit 7c30f352c852 ("jiffies.h: declare jiffies and jiffies_64 with
    ____cacheline_aligned_in_smp") removed a section specification from the
    jiffies declaration that caused conflicts on some platforms.
    
    Unfortunately this change broke the build for frv:
    
      kernel/built-in.o: In function `__do_softirq': (.text+0x6460): relocation truncated to fit: R_FRV_GPREL12 against symbol
          `jiffies' defined in *ABS* section in .tmp_vmlinux1
      kernel/built-in.o: In function `__do_softirq': (.text+0x6574): relocation truncated to fit: R_FRV_GPREL12 against symbol
          `jiffies' defined in *ABS* section in .tmp_vmlinux1
      kernel/built-in.o: In function `pwq_activate_delayed_work': workqueue.c:(.text+0x15b9c): relocation truncated to fit: R_FRV_GPREL12 against
          symbol `jiffies' defined in *ABS* section in .tmp_vmlinux1
      ...
    
    Add __jiffy_arch_data to the declaration of jiffies and use it on frv to
    include the section specification.  For all other platforms
    __jiffy_arch_data (currently) has no effect.
    
    Fixes: 7c30f352c852 ("jiffies.h: declare jiffies and jiffies_64 with ____cacheline_aligned_in_smp")
    Link: http://lkml.kernel.org/r/20170516221333.177280-1-mka@chromium.org
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: David Howells <dhowells@redhat.com>
    Cc: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 60b0a8c3d2480f3b57282b47b7cae7ee71c48635)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/jiffies.h
+++ include/linux/jiffies.h
@@ -67,13 +67,17 @@
 /* a value TUSEC for TICK_USEC (can be set bij adjtimex)		*/
 #define TICK_USEC_TO_NSEC(TUSEC) (SH_DIV (TUSEC * USER_HZ * 1000, ACTHZ, 8))
 
+#ifndef __jiffy_arch_data
+#define __jiffy_arch_data
+#endif
+
 /*
  * The 64-bit value is not atomic - you MUST NOT read it
  * without sampling the sequence number in xtime_lock.
  * get_jiffies_64() will do this for you as appropriate.
  */
 extern u64 __cacheline_aligned_in_smp jiffies_64;
-extern unsigned long volatile __cacheline_aligned_in_smp jiffies;
+extern unsigned long volatile __cacheline_aligned_in_smp __jiffy_arch_data jiffies;
 
 #if (BITS_PER_LONG < 64)
 u64 get_jiffies_64(void);
