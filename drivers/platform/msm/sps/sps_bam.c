f52fdfd40c6900c0af5df2c9b89c1012841ab9e1
Gobinda Joy <gobinda.joy@gmail.com>
2017-06-20 09:07:39 +0600

    driver:platform:msm:sps msm: sps: Fix register SPS IRQ
    # Conflicts:
    #	drivers/platform/msm/sps/sps_bam.c

--- drivers/platform/msm/sps/sps_bam.c
+++ drivers/platform/msm/sps/sps_bam.c
@@ -287,7 +287,7 @@ int sps_bam_enable(struct sps_bam *dev)
 					IRQF_TRIGGER_RISING | IRQF_NO_SUSPEND,
 					"sps", dev);
 				SPS_DBG(
-					"sps:BAM %pa uses edge for IRQ# %d\n",
+					"sps:BAM 0x%x uses edge for IRQ# %d\n",
 					BAM_ID(dev), dev->props.irq);
 			} else {
 				result = request_irq(dev->props.irq,
@@ -295,11 +295,11 @@ int sps_bam_enable(struct sps_bam *dev)
 					IRQF_TRIGGER_HIGH | IRQF_NO_SUSPEND,
 					"sps", dev);
 				SPS_DBG(
-					"sps:BAM %pa uses level for IRQ# %d\n",
+					"sps:BAM 0x%x uses level for IRQ# %d\n",
 					BAM_ID(dev), dev->props.irq);
 			}
 		} else {
-			SPS_DBG1("sps:BAM %pa does not have an vaild IRQ# %d\n",
+			SPS_DBG1("sps:BAM 0x%x does not have an vaild IRQ# %d\n",
 				BAM_ID(dev), dev->props.irq);
 		}
 
