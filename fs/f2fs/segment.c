7378ee790a9623e4549eced5a68b9f89828e52ee
Alexander Potapenko <glider@google.com>
2017-07-19 20:27:30 +0200

    llist: clang: introduce member_address_is_nonnull()
    Currently llist_for_each_entry() and llist_for_each_entry_safe() iterate
    until &pos->member != NULL.  But when building the kernel with Clang,
    the compiler assumes &pos->member cannot be NULL if the member's offset
    is greater than 0 (which would be equivalent to the object being
    non-contiguous in memory).  Therefore the loop condition is always true,
    and the loops become infinite.
    
    To work around this, introduce the member_address_is_nonnull() macro,
    which casts object pointer to uintptr_t, thus letting the member pointer
    to be NULL.
    
    Change-Id: I1f9541591bba1f2ca16ecbb0248555298df4bee5
    Fixes: 64082472
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Tested-by: Sodagudi Prasad <psodagud@codeaurora.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -31,6 +31,23 @@ static struct kmem_cache *sit_entry_set_slab;
 static struct kmem_cache *inmem_entry_slab;
 
 /**
+ * member_address_is_nonnull - check whether the member address is not NULL
+ * @ptr: the object pointer (struct type * that contains the llist_node)
+ * @member: the name of the llist_node within the struct.
+ *
+ * This macro is conceptually the same as
+ * &ptr->member != NULL
+ * but it works around the fact that compilers can decide that taking a member
+ * address is never a NULL pointer.
+ *
+ * Real objects that start at a high address and have a member at NULL are
+ * unlikely to exist, but such pointers may be returned e.g. by the
+ * container_of() macro.
+ */
+#define member_address_is_nonnull(ptr, member) \
+((uintptr_t)(ptr) + offsetof(typeof(*(ptr)), member) != 0)
+
+/**
  * Copied from latest lib/llist.c
  * llist_for_each_entry_safe - iterate over some deleted entries of
  *                             lock-less list of given type
@@ -51,7 +68,7 @@ static struct kmem_cache *inmem_entry_slab;
  */
 #define llist_for_each_entry_safe(pos, n, node, member)			       \
 	for (pos = llist_entry((node), typeof(*pos), member);		       \
-		&pos->member != NULL &&					       \
+		member_address_is_nonnull(pos, member) &&			\
 		(n = llist_entry(pos->member.next, typeof(*n), member), true); \
 		pos = n)
 
