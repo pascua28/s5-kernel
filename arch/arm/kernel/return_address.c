b17b1aefc6c519475dfde6ba9de19bca42266074
sunilpaulmathew <sunil.kde@gmail.com>
2017-11-01 12:30:21 +0900

    Some modification to work with higher versions...
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -55,10 +55,6 @@ void *return_address(unsigned int level)
 	else
 		return NULL;
 }
-
-#else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
-
-
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
 
 EXPORT_SYMBOL_GPL(return_address);
