fe81abc0f04cb1bf82a65def5d62bc5c68a715e8
mostafa-z <mostafazarghami@gmail.com>
2018-01-10 14:03:18 +0330

    mm:zcache: tune
    * Enable by default. If you're compiling it, you will use it.
    * Use LZ4 crypto compressor, if available.
    
    thanks @neobuddy89
    
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- mm/zcache.c
+++ mm/zcache.c
@@ -40,16 +40,20 @@
 #include <linux/zbud.h>
 
 /*
- * Enable/disable zcache (disabled by default)
+ * Enable/disable zcache (enabled by default)
  */
-static bool zcache_enabled __read_mostly;
+static bool zcache_enabled = true;
 module_param_named(enabled, zcache_enabled, bool, 0);
 
 /*
  * Compressor to be used by zcache
  */
 #define ZCACHE_COMPRESSOR_DEFAULT "lzo"
+#ifndef CONFIG_CRYPTO_LZ4
 static char *zcache_compressor = ZCACHE_COMPRESSOR_DEFAULT;
+#else
+static char *zcache_compressor = "lz4";
+#endif
 module_param_named(compressor, zcache_compressor, charp, 0);
 
 /*
