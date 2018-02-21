fe7fe53a838518fec125c2b3f12074d4fcf273c0
Cao jin <caoj.fnst@cn.fujitsu.com>
2018-02-21 12:25:07 +0800

    kbuild: drop superfluous GCC_PLUGINS_CFLAGS assignment
    GCC_PLUGINS_CFLAGS is already in the environment, so it is superfluous
    to add it in commandline of final build of init/.
    
    Signed-off-by: Cao jin <caoj.fnst@cn.fujitsu.com>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -240,7 +240,7 @@ else
 fi;
 
 # final build of init/
-${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init GCC_PLUGINS_CFLAGS="${GCC_PLUGINS_CFLAGS}"
+${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init
 
 archive_builtin
 
