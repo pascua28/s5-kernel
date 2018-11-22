44706f21778e4ec0a498d6c7d56431e043a04807
Haibin Liu <haibinl@codeaurora.org>
2018-11-22 16:23:57 +0800

    msm: camera: Fix out of bound read access
    Input num_streams cannot be greater than the max allowed
    number of streams, otherwise this casue OOB read access.
    
    Add bounds check for num_streams which user input.
    
    CRs-Fixed: 2330040
    Change-Id: I76fb785dc54c597603d748d604844952cea659ea
    Signed-off-by: Haibin Liu <haibinl@codeaurora.org>
    Signed-off-by: Khalvat-M <mmba313@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -912,6 +912,10 @@ msm_isp_get_camif_update_state(struct vfe_device *vfe_dev,
 	cur_pix_stream_cnt =
 		axi_data->src_info[VFE_PIX_0].pix_stream_count +
 		axi_data->src_info[VFE_PIX_0].raw_stream_count;
+
+	if (stream_cfg_cmd->num_streams > VFE_AXI_SRC_MAX)
+		return NO_UPDATE;
+
 	for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 		stream_info =
 			&axi_data->stream_info[
