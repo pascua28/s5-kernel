60e7106a842146c548dcbf485f5d9a9bd6523cd4
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-05 20:54:09 +0800

    update build scripts

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,15 @@ export CROSS_COMPILE=~/ubertc/bin/arm-eabi-
 
 mkdir output
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+if [ -e output/.config ]
+    then
+        make -j4 O=output oldconfig
+    else
+        cp defconfig output/.config
+        make -j4 O=output oldconfig
+fi
+
+make -j4 O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+tools/dtbTool -2 -o output/arch/arm/boot/dt.img -s 2048 -p output/scripts/dtc/ output/arch/arm/boot/
