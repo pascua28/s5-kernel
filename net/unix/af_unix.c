e5317fcea48a15a494e5c74413dbbc325d03f641
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-19 17:25:30 +0800

    Revert "unix: fix a race condition in unix_release()"
    This reverts commit 3a0ebbb7d06f594adeebde59b102a0c592fbfbd0.

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -486,7 +486,7 @@ static void unix_sock_destructor(struct sock *sk)
 #endif
 }
 
-static void unix_release_sock(struct sock *sk, int embrion)
+static int unix_release_sock(struct sock *sk, int embrion)
 {
 	struct unix_sock *u = unix_sk(sk);
 	struct path path;
@@ -557,6 +557,8 @@ static void unix_release_sock(struct sock *sk, int embrion)
 
 	if (unix_tot_inflight)
 		unix_gc();		/* Garbage collect fds */
+
+	return 0;
 }
 
 static void init_peercred(struct sock *sk)
@@ -807,10 +809,9 @@ static int unix_release(struct socket *sock)
 	if (!sk)
 		return 0;
 
-	unix_release_sock(sk, 0);
 	sock->sk = NULL;
 
-	return 0;
+	return unix_release_sock(sk, 0);
 }
 
 static int unix_autobind(struct socket *sock)
