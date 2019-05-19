db7814d4f40430fa59ece9dec31676d287789e09
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-19 12:35:49 +0800

    ipv6: drop accept_ra_mtu

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -173,7 +173,6 @@ struct ipv6_devconf {
 	__s32		accept_dad;
 	__s32		force_tllao;
 	__s32		accept_ra_prefix_route;
-	__s32		accept_ra_mtu;
 	void		*sysctl;
 };
 
@@ -216,7 +215,6 @@ enum {
 	DEVCONF_ACCEPT_DAD,
 	DEVCONF_FORCE_TLLAO,
 	DEVCONF_ACCEPT_RA_PREFIX_ROUTE,
-	DEVCONF_ACCEPT_RA_MTU,
 	DEVCONF_MAX
 };
 
