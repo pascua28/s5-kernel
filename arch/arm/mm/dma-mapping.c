fce7fd47800595a2eaa657df809c9469a5713bd7
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-21 19:48:26 +0800

    Revert "ARM: fix executability of CMA mappings"
    This reverts commit 784b03235dc0d07c354083c48082e40eeebd4bbd.

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -317,7 +317,7 @@ early_param("coherent_pool", early_coherent_pool);
 static int __init atomic_pool_init(void)
 {
 	struct dma_pool *pool = &atomic_pool;
-	pgprot_t prot = pgprot_dmacoherent(PAGE_KERNEL);
+	pgprot_t prot = pgprot_dmacoherent(pgprot_kernel);
 	unsigned long nr_pages = pool->size >> PAGE_SHIFT;
 	unsigned long *bitmap;
 	struct page *page;
@@ -559,7 +559,7 @@ static void __free_from_contiguous(struct device *dev, struct page *page,
 				   void *cpu_addr, size_t size)
 {
 	if (!PageHighMem(page))
-		__dma_remap(page, size, PAGE_KERNEL, false);
+		__dma_remap(page, size, pgprot_kernel, false);
 	else
 		__dma_free_remap(cpu_addr, size, true);
 	dma_release_from_contiguous(dev, page, size >> PAGE_SHIFT);
@@ -1242,7 +1242,7 @@ static struct page **__iommu_get_pages(void *cpu_addr)
 static void *arm_iommu_alloc_attrs(struct device *dev, size_t size,
 	    dma_addr_t *handle, gfp_t gfp, struct dma_attrs *attrs)
 {
-	pgprot_t prot = __get_dma_pgprot(attrs, PAGE_KERNEL);
+	pgprot_t prot = __get_dma_pgprot(attrs, pgprot_kernel);
 	struct page **pages;
 	void *addr = NULL;
 
