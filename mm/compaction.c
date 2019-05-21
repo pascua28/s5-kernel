47609efc6b4854ed90c810e46303fc44c8d59fcf
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-21 19:55:47 +0800

    Revert "mm/compaction: Break out of loop on !PageBuddy in isolate_freepages_block"
    This reverts commit 7a93e741a12e57c882db441d11dae05a220dffdb.

--- mm/compaction.c
+++ mm/compaction.c
@@ -243,6 +243,7 @@ static unsigned long isolate_freepages_block(struct compact_control *cc,
 {
 	int nr_scanned = 0, total_isolated = 0;
 	struct page *cursor, *valid_page = NULL;
+	unsigned long nr_strict_required = end_pfn - blockpfn;
 	unsigned long flags;
 	bool locked = false;
 
@@ -255,12 +256,11 @@ static unsigned long isolate_freepages_block(struct compact_control *cc,
 
 		nr_scanned++;
 		if (!pfn_valid_within(blockpfn))
-			goto isolate_fail;
-
+			continue;
 		if (!valid_page)
 			valid_page = page;
 		if (!PageBuddy(page))
-			goto isolate_fail;
+			continue;
 
 		/*
 		 * The zone lock must be held to isolate freepages.
@@ -281,10 +281,12 @@ static unsigned long isolate_freepages_block(struct compact_control *cc,
 
 		/* Recheck this is a buddy page under lock */
 		if (!PageBuddy(page))
-			goto isolate_fail;
+			continue;
 
 		/* Found a free page, break it into order-0 pages */
 		isolated = split_free_page(page);
+		if (!isolated && strict)
+			break;
 		total_isolated += isolated;
 		for (i = 0; i < isolated; i++) {
 			list_add(&page->lru, freelist);
@@ -295,13 +297,7 @@ static unsigned long isolate_freepages_block(struct compact_control *cc,
 		if (isolated) {
 			blockpfn += isolated - 1;
 			cursor += isolated - 1;
-			continue;
 		}
-
-isolate_fail:
-		if (strict)
-			break;
-
 	}
 
 	trace_mm_compaction_isolate_freepages(nr_scanned, total_isolated);
@@ -311,7 +307,7 @@ isolate_fail:
 	 * pages requested were isolated. If there were any failures, 0 is
 	 * returned and CMA will fail.
 	 */
-	if (strict && blockpfn < end_pfn)
+	if (strict && nr_strict_required > total_isolated)
 		total_isolated = 0;
 
 	if (locked)
