e1203debdcacdcc2e73088ec2f726f61000971a2
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-21 19:47:07 +0800

    Revert "ext4: limit the number of error prints"
    This reverts commit 00644a466e797a4849e32bbe64244872de04c96d.

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -43,7 +43,6 @@
 
 #include <linux/kthread.h>
 #include <linux/freezer.h>
-#include <linux/ratelimit.h>
 
 #include "ext4.h"
 #include "ext4_extents.h"
@@ -709,7 +708,7 @@ void ext4_msg(struct super_block *sb, const char *prefix, const char *fmt, ...)
 	va_start(args, fmt);
 	vaf.fmt = fmt;
 	vaf.va = &args;
-	printk_ratelimited("%sEXT4-fs (%s): %pV\n", prefix, sb->s_id, &vaf);
+	printk("%sEXT4-fs (%s): %pV\n", prefix, sb->s_id, &vaf);
 	va_end(args);
 }
 
