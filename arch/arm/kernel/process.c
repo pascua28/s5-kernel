990cde6d8ca8f796dada23fdcfeca962dc179074
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-05-21 17:29:12 +0800

    Revert "ARM: Improve vmalloc address checking in show_data()"
    This reverts commit 2811c0946b2164cc3ede5350ce04dae32ce863e2.

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -354,10 +354,12 @@ static void show_data(unsigned long addr, int nbytes, const char *name)
 	u32	*p;
 
 	/*
-	 * don't attempt to dump non-kernel addresses or
-	 * values that are probably just small negative numbers
+	 * don't attempt to dump non-kernel addresses, values that are probably
+	 * just small negative numbers, or vmalloc addresses that may point to
+	 * memory-mapped peripherals
 	 */
-	if (addr < PAGE_OFFSET || addr > -256UL)
+	if (addr < PAGE_OFFSET || addr > -256UL ||
+	    is_vmalloc_addr((void *)addr))
 		return;
 
 	if (is_vmalloc_addr((void *)addr))
@@ -389,12 +391,7 @@ static void show_data(unsigned long addr, int nbytes, const char *name)
 		printk("%04lx ", (unsigned long)p & 0xffff);
 		for (j = 0; j < 8; j++) {
 			u32	data;
-			/*
-			 * vmalloc addresses may point to
-			 * memory-mapped peripherals
-			 */
-			if (is_vmalloc_addr(p) ||
-			    probe_kernel_address(p, data)) {
+			if (probe_kernel_address(p, data)) {
 				printk(" ********");
 			} else {
 				printk(" %08x", data);
