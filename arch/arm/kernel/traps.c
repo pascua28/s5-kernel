23bc0ea268ac465ca9fcabb391bc0cf031b698a6
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-06-05 09:32:00 +0800

    Revert "ARM: Allow panic on division by zero in the kernel"
    This reverts commit 9c6b64ed6178935d00733b47f7055c748efa805c.

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -25,7 +25,6 @@
 #include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/sched.h>
-#include <linux/bug.h>
 
 #include <linux/atomic.h>
 #include <asm/cacheflush.h>
@@ -791,7 +790,6 @@ void __pgd_error(const char *file, int line, pgd_t pgd)
 asmlinkage void __div0(void)
 {
 	printk("Division by zero in kernel.\n");
-	BUG_ON(PANIC_CORRUPTION);
 	dump_stack();
 }
 EXPORT_SYMBOL(__div0);
