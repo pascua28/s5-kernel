8556d0d29bebf7291d0782c1e8b1bd0b6edd28eb
Jan Forman <info@janforman.com>
2019-06-19 07:39:52 +0000

    mm: introduce vma_is_anonymous(vma) helper
    Change-Id: Ie8ad02ead246527ce895d0a0a98bf1a99de8464b

--- mm/memory.c
+++ mm/memory.c
@@ -3693,7 +3693,11 @@ int handle_pte_fault(struct mm_struct *mm,
 	entry = *pte;
 	if (!pte_present(entry)) {
 		if (pte_none(entry)) {
-			if (vma->vm_ops)
+
+		    if (vma_is_anonymous(vma))
+			return do_anonymous_page(mm, vma, address,
+			pte, pmd, flags);
+		    else
 				return do_linear_fault(mm, vma, address, pte, pmd,
 						flags, entry);
 
