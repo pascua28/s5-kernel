7abbfe63289d33539d6b3c63e766675f16735412
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-08-13 18:18:54 +0800

    f2fs: make more closer to 4.11-rc1-3.18

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -214,11 +214,9 @@ static int f2fs_set_acl(struct inode *inode, int type,
 		name_index = F2FS_XATTR_INDEX_POSIX_ACL_ACCESS;
 		if (acl) {
 			error = posix_acl_equiv_mode(acl, &inode->i_mode);
-			if (error < 0)
+			if (error)
 				return error;
 			set_acl_inode(inode, inode->i_mode);
-			if (error == 0)
-				acl = NULL;
 		}
 		break;
 
