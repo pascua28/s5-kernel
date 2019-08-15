fb63a4fe1f517fc2a824cc8602e55861e5d87668
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-08-15 20:35:13 +0800

    clang: add some gcc prebuilts

--- clang.sh
+++ clang.sh
@@ -11,11 +11,16 @@ if [ -e output/.config ]
 fi
 
 DATE_START=$(date +"%s")
+
+git apply gcc_prebuilts
+
 make -j$(nproc --all) \
 ARCH=arm CC="ccache /home/pascua14/clang/bin/clang" \
 CLANG_TRIPLE=arm-linux-gnueabihf- \
 CROSS_COMPILE=/home/pascua14/arm32/bin/arm-linux-gnueabihf- \
 O=output
+
+git apply -R gcc_prebuilts
 
 tools/dtbTool -2 -o output/arch/arm/boot/dt.img -s 2048 -p output/scripts/dtc/ output/arch/arm/boot/
 
