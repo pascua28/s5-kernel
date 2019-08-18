38de7be97064411b61313e2bb89573062df5f400
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-08-18 13:48:08 +0800

    dump build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=~/arm32/bin/arm-linux-gnueabihf-
 
-mkdir output
+#make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+if [ -e .config ]
+    then
+        make -j4 oldconfig
+    else
+        cp defconfig.config
+        make -j4 oldconfig
+fi
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+make -j4 2>&1 |tee ../compile.log
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+tools/dtbTool -2 -o arch/arm/boot/dt.img -s 2048 -p scripts/dtc/ arch/arm/boot/
