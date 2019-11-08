fd16ec3de26616b7db5ca6f9a3080a541730e8b2
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-11-08 18:11:04 +0800

    intellikernel.sh: set rqbalance as CPUQuiet governor

--- build/ramdisk/sbin/intellikernel.sh
+++ build/ramdisk/sbin/intellikernel.sh
@@ -23,3 +23,6 @@ echo '1024' > /sys/block/mmcblk0/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk0/queue/iostats
 echo '2048' > /sys/block/mmcblk1/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk1/queue/iostats
+
+# CPUQuiet
+echo "rqbalance" > /sys/devices/system/cpu/cpuquiet/current_governor
