9f811866c2bc5b09f36673fdf103361c90efdfdc
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-11-15 07:56:19 +0800

    ramdisk: default to mpdecision

--- build/anykernel.sh
+++ build/anykernel.sh
@@ -78,8 +78,6 @@ remove_section init.qcom.rc "service tweaks" "seclabel u:r:magisk:s0"
 
 remove_line init.qcom.rc "start tweaks"
 
-replace_line init.qcom.rc "start mpdecision" "\t# start mpdecision";
-
 insert_line init.qcom.rc "exec u:r:magisk:s0 root root -- /sbin/intellikernel.sh" after "start mpdecision" "\texec u:r:magisk:s0 root root -- /sbin/intellikernel.sh";
 
 insert_line init.qcom.rc "exec u:r:supersu:s0 root root -- /sbin/intellikernel.sh" after "start mpdecision" "\texec u:r:supersu:s0 root root -- /sbin/intellikernel.sh";
