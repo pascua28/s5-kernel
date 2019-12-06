b26f67aef08276f4a99fd9978b5e6b097a20a4ab
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-12-06 22:09:36 +0800

    add clean script

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git ls-files . --ignored --exclude-standard --others --directory | while read file; do echo $file; rm -rf $file; done
+git ls-files . --exclude-standard --others --directory | while read file; do echo $file; rm -rf $file; done
