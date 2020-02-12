9caa606e310ce3053e9ed6e76d34c37bac19d4bd
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-02-13 01:05:00 +0800

    intellikernel.sh: SafetyNet patch

--- build/ramdisk/overlay.d/sbin/intellikernel.sh
+++ build/ramdisk/overlay.d/sbin/intellikernel.sh
@@ -10,6 +10,10 @@ sysctl -w vm.swappiness=130
 # Disable adaptive lmk
 echo '0' > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 
+# SafetyNet
+chmod 640 /sys/fs/selinux/enforce;
+chmod 440 /sys/fs/selinux/policy;
+
 # Set I/O scheduler back to CFQ
 setprop sys.io.scheduler cfq
 echo '1024' > /sys/block/mmcblk0/queue/read_ahead_kb
