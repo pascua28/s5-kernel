dc9fe981e8eb9f3c5fa5ee6686073a88118cafff
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-02-18 19:03:40 +0800

    intelliactive: update utilization

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -437,6 +437,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 			phase = 1;
 		}
 	}
+	cpufreq_notify_utilization(pcpu->policy, cpu_load);
 
 	if (cpu_load >= go_hispeed_load || boosted) {
 		if (pcpu->target_freq < hispeed_freq) {
