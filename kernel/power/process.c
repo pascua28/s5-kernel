6512fdf226de389e21c7775736c0992ffa074f1b
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-02-28 18:15:48 +0800

    Revert "power: Remove legacy wakelock code."
    This reverts commit f85607a715a74c65db812cd3901022888257f966.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -28,7 +28,6 @@
 static int try_to_freeze_tasks(bool user_only)
 {
 	struct task_struct *g, *p;
-	struct task_struct *q = NULL;
 	unsigned long end_time;
 	unsigned int todo;
 	bool wq_busy = false;
@@ -62,10 +61,8 @@ static int try_to_freeze_tasks(bool user_only)
 			 * transition can't race with task state testing here.
 			 */
 			if (!task_is_stopped_or_traced(p) &&
-			    !freezer_should_skip(p)) {
+			    !freezer_should_skip(p))
 				todo++;
-				q = p;
-			}
 		} while_each_thread(g, p);
 		read_unlock(&tasklist_lock);
 
@@ -74,6 +71,10 @@ static int try_to_freeze_tasks(bool user_only)
 			todo += wq_busy;
 		}
 
+		if (todo && has_wake_lock(WAKE_LOCK_SUSPEND)) {
+			wakeup = 1;
+			break;
+		}
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
@@ -102,9 +103,8 @@ static int try_to_freeze_tasks(bool user_only)
 		 */
 		if(wakeup) {
 			printk("\n");
-			printk(KERN_ERR "Freezing of %s aborted (%d) (%s)\n",
-					user_only ? "user space " : "tasks ",
-					q ? q->pid : 0, q ? q->comm : "NONE");
+			printk(KERN_ERR "Freezing of %s aborted\n",
+					user_only ? "user space " : "tasks ");
 		}
 		else {
 			printk("\n");
@@ -176,6 +176,10 @@ int freeze_processes(void)
 int freeze_kernel_threads(void)
 {
 	int error;
+
+	error = suspend_sys_sync_wait();
+	if (error)
+		return error;
 
 	printk("Freezing remaining freezable tasks ... ");
 	pm_nosig_freezing = true;
