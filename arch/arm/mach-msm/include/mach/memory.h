805e079ac5786a803034ccdada436726129b9b91
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-02-28 15:13:57 +0800

    Revert "msm: memory: Use phys_addr_t as the return type for ebi functions"
    This reverts commit 771c3045b1c4f30b70e79bbd439c8e21e17ff0ca.

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -70,7 +70,7 @@ extern unsigned long ebi1_phys_offset;
 
 #ifndef __ASSEMBLY__
 void *allocate_contiguous_ebi(unsigned long, unsigned long, int);
-phys_addr_t allocate_contiguous_ebi_nomap(unsigned long, unsigned long);
+unsigned long allocate_contiguous_ebi_nomap(unsigned long, unsigned long);
 void clean_and_invalidate_caches(unsigned long, unsigned long, unsigned long);
 void clean_caches(unsigned long, unsigned long, unsigned long);
 void invalidate_caches(unsigned long, unsigned long, unsigned long);
