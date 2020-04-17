0dcb889fb66dfbcd1068e76d2f996f53d1bfd931
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-04-17 10:44:01 +0800

    Revert "lib: genalloc: Change chunk allocation to vmalloc"
    This reverts commit e461457b134e55c3ca68b91b70b8796e3f3dba69.

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -34,7 +34,6 @@
 #include <linux/rculist.h>
 #include <linux/interrupt.h>
 #include <linux/genalloc.h>
-#include <linux/vmalloc.h>
 
 static int set_bits_ll(unsigned long *addr, unsigned long mask_to_set)
 {
@@ -177,16 +176,11 @@ int gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, phys_addr_t phy
 	struct gen_pool_chunk *chunk;
 	int nbits = size >> pool->min_alloc_order;
 	int nbytes = sizeof(struct gen_pool_chunk) +
-				BITS_TO_LONGS(nbits) * sizeof(long);
+				(nbits + BITS_PER_BYTE - 1) / BITS_PER_BYTE;
 
-	if (nbytes <= PAGE_SIZE)
-		chunk = kmalloc_node(nbytes, __GFP_ZERO, nid);
-	else
-		chunk = vmalloc(nbytes);
+	chunk = kmalloc_node(nbytes, GFP_KERNEL | __GFP_ZERO, nid);
 	if (unlikely(chunk == NULL))
 		return -ENOMEM;
-	if (nbytes > PAGE_SIZE)
-		memset(chunk, 0, nbytes);
 
 	chunk->phys_addr = phys;
 	chunk->start_addr = virt;
@@ -241,20 +235,14 @@ void gen_pool_destroy(struct gen_pool *pool)
 	int bit, end_bit;
 
 	list_for_each_safe(_chunk, _next_chunk, &pool->chunks) {
-		int nbytes;
 		chunk = list_entry(_chunk, struct gen_pool_chunk, next_chunk);
 		list_del(&chunk->next_chunk);
 
 		end_bit = (chunk->end_addr - chunk->start_addr) >> order;
-		nbytes = sizeof(struct gen_pool_chunk) +
-				BITS_TO_LONGS(end_bit) * sizeof(long);
 		bit = find_next_bit(chunk->bits, end_bit, 0);
 		BUG_ON(bit < end_bit);
 
-		if (nbytes <= PAGE_SIZE)
-			kfree(chunk);
-		else
-			vfree(chunk);
+		kfree(chunk);
 	}
 	kfree(pool);
 	return;
