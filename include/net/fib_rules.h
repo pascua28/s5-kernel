120a37d5b2002b7bd1f1c5bfd49901a524302d7a
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-04-28 19:47:49 +0800

    fix derp

--- include/net/fib_rules.h
+++ include/net/fib_rules.h
@@ -90,7 +90,7 @@ struct fib_rules_ops {
 	[FRA_TABLE]     = { .type = NLA_U32 }, \
 	[FRA_GOTO]	= { .type = NLA_U32 }, \
 	[FRA_UID_START]	= { .type = NLA_U32 }, \
-	[FRA_UID_END]	= { .type = NLA_U32 }
+	[FRA_UID_END]	= { .type = NLA_U32 }, \
 	[FRA_UID_RANGE]	= { .len = sizeof(struct fib_rule_uid_range) }
 
 static inline void fib_rule_get(struct fib_rule *rule)
