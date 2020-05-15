0dbae9994497bb0277e16aaa529d57907dd489e8
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-05-15 09:29:50 +0800

    ARM: remove IPI arch_trigger_all_cpu_backtrace

--- arch/arm/include/asm/irq.h
+++ arch/arm/include/asm/irq.h
@@ -30,9 +30,6 @@ extern void asm_do_IRQ(unsigned int, struct pt_regs *);
 void handle_IRQ(unsigned int, struct pt_regs *);
 void init_IRQ(void);
 
-void arch_trigger_all_cpu_backtrace(void);
-#define arch_trigger_all_cpu_backtrace arch_trigger_all_cpu_backtrace
-
 #endif
 
 #endif
