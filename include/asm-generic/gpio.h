dab7de185840ab429890062e1a48403c7c8eb7db
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-05-15 19:41:03 +0800

    Revert "gpiolib: let gpio_chip reference its descriptors"
    This reverts commit 6c0b4e6c85d085bd92966bc2b8da73e2d7f35929.

--- include/asm-generic/gpio.h
+++ include/asm-generic/gpio.h
@@ -49,7 +49,6 @@ struct gpio;
 struct seq_file;
 struct module;
 struct device_node;
-struct gpio_desc;
 
 /**
  * struct gpio_chip - abstract a GPIO controller
@@ -79,7 +78,6 @@ struct gpio_desc;
  *	negative during registration, requests dynamic ID allocation.
  * @ngpio: the number of GPIOs handled by this controller; the last GPIO
  *	handled is (base + ngpio - 1).
- * @desc: array of ngpio descriptors. Private.
  * @can_sleep: flag must be set iff get()/set() methods sleep, as they
  *	must while accessing GPIO expander chips over I2C or SPI
  * @names: if set, must be an array of strings to use as alternative
@@ -130,7 +128,6 @@ struct gpio_chip {
 						struct gpio_chip *chip);
 	int			base;
 	u16			ngpio;
-	struct gpio_desc	*desc;
 	const char		*const *names;
 	unsigned		can_sleep:1;
 	unsigned		exported:1;
