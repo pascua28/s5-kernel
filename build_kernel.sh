9f5706d90069ef5258b2c5d2d1531fb0d7e3e900
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-05-18 16:51:28 +0800

    script: show compile time

--- build_kernel.sh
+++ build_kernel.sh
@@ -13,8 +13,15 @@ if [ -e output/.config ]
         make -j4 O=output oldconfig
 fi
 
+DATE_START=$(date +"%s")
+
 make -j4 CONFIG_DEBUG_SECTION_MISMATCH=y \
 	O=output 2>&1 | tee ../compile.log
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
 tools/dtbTool -2 -o output/arch/arm/boot/dt.img -s 2048 -p output/scripts/dtc/ output/arch/arm/boot/
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
