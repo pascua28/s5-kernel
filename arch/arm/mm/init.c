a540070c32db6e551a5965d11ec71d155693f8d1
Samuel Pascua <sgpascua@ngcp.ph>
2020-09-18 13:19:06 +0800

    arm: remove STRICT_MEMORY_RWX option
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -876,18 +876,9 @@ void free_initmem(void)
 	free_reserved_area(&__tcm_start, &__tcm_end, -1, "TCM link");
 #endif
 
-#ifdef CONFIG_STRICT_MEMORY_RWX
-	poison_init_mem((char *)__arch_info_begin,
-		__init_end - (char *)__arch_info_begin);
-	reclaimed_initmem = free_area(__phys_to_pfn(__pa(__arch_info_begin)),
-				    __phys_to_pfn(__pa(__init_end)),
-				    "init");
-	totalram_pages += reclaimed_initmem;
-#else
 	poison_init_mem(__init_begin, __init_end - __init_begin);
 	if (!machine_is_integrator() && !machine_is_cintegrator())
 		free_initmem_default(-1);
-#endif
 }
 
 #ifdef CONFIG_BLK_DEV_INITRD
