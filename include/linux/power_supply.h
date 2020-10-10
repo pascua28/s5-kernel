73c649844a55ff7ad13005e133e346d66f2134f3
Samuel Pascua <sgpascua@ngcp.ph>
2020-10-10 17:06:08 +0800

    Revert "Revert "power_supply: Hold a wake_lock while power supply change notifications are pending""
    This reverts commit 8d41562200023efb4999effdf6bdaad8353aa490.

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -13,6 +13,7 @@
 #ifndef __LINUX_POWER_SUPPLY_H__
 #define __LINUX_POWER_SUPPLY_H__
 
+#include <linux/wakelock.h>
 #include <linux/workqueue.h>
 #include <linux/leds.h>
 
@@ -242,10 +243,13 @@ struct power_supply {
 	/* private */
 	struct device *dev;
 	struct work_struct changed_work;
+	spinlock_t changed_lock;
+	bool changed;
 #ifdef CONFIG_THERMAL
 	struct thermal_zone_device *tzd;
 	struct thermal_cooling_device *tcd;
 #endif
+	struct wake_lock work_wake_lock;
 
 #ifdef CONFIG_LEDS_TRIGGERS
 	struct led_trigger *charging_full_trig;
