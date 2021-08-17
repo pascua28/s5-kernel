92b28b9185d6fcef769dddff33bdf5c0fa6ed0a6
Khalvat-M <mmba313@gmail.com>
2021-08-17 23:21:53 +0430

    net: msm_rmnet: Disable too much logspam
    [rmnet0] error: rmnet_ioct called for unsupported cmd[35585]

--- drivers/net/ethernet/msm/msm_rmnet.c
+++ drivers/net/ethernet/msm/msm_rmnet.c
@@ -717,7 +717,7 @@ static int rmnet_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 		break;
 
 	default:
-		pr_err("[%s] error: rmnet_ioct called for unsupported cmd[%d]",
+		pr_dbg("[%s] error: rmnet_ioct called for unsupported cmd[0x%x]\n",
 			dev->name, cmd);
 		return -EINVAL;
 	}
