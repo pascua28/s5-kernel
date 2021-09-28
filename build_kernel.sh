c4f4f74de09c572058d0417c3c3ed0ba9e17c45d
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-09-28 08:55:36 +0800

    add build env
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=~/ubertc/bin/arm-eabi-
 
 mkdir output
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+make -C $(pwd) O=output lineage_klte_pn547_defconfig
+make -j8 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+
+tools/dtbTool -2 -o output/arch/arm/boot/dt.img -s 2048 -p output/scripts/dtc/ output/arch/arm/boot/
