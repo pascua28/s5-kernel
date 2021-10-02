moetayuko/CAF_kernel_msm-3.18

usb: replace %p with %pK
Format specifier %p can leak kernel addresses while not valuing the
kptr_restrict system settings. When kptr_restrict is set to (1), kernel
pointers printed using the %pK format specifier will be replaced with 0's.
Debugging Note : &pK prints only Zeros as address. If you need actual
address information, write 0 to kptr_restrict.
echo 0 > /proc/sys/kernel/kptr_restrict

CRs-fixed: 1052849
Change-Id: I0e98145730380ea983fa8f46a28d15dd6c2c31df
Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
Signed-off-by: Yasir Malik <ymalik@codeaurora.org>

drivers/usb/host/ehci-fsl.c
@@ -144,7 +144,7 @@ static int usb_hcd_fsl_probe(const struct hc_driver *driver,
 		struct ehci_hcd *ehci = hcd_to_ehci(hcd);
 
 		hcd->phy = usb_get_phy(USB_PHY_TYPE_USB2);
-		dev_dbg(&pdev->dev, "hcd=0x%p  ehci=0x%p, phy=0x%p\n",
+		dev_dbg(&pdev->dev, "hcd=0x%pK  ehci=0x%pK, phy=0x%pK\n",
 			hcd, ehci, hcd->phy);
 
 		if (!IS_ERR_OR_NULL(hcd->phy)) {

pascua28/s5-kernel

Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

drivers/usb/host/ehci-fsl.c
@@ -143,7 +143,7 @@ static int usb_hcd_fsl_probe(const struct hc_driver *driver,
 		struct ehci_hcd *ehci = hcd_to_ehci(hcd);
 
 		ehci->transceiver = usb_get_transceiver();
-		dev_dbg(&pdev->dev, "hcd=0x%p  ehci=0x%p, transceiver=0x%p\n",
+		dev_dbg(&pdev->dev, "hcd=0x%pK  ehci=0x%pK, transceiver=0x%pK\n",
 			hcd, ehci, ehci->transceiver);
 
 		if (ehci->transceiver) {