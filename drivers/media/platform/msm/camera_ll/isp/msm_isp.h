LineageOS/android_kernel_samsung_msm8974

drivers: media: platform: msm: camera_ll: Add lt03lte compatibility
* Largely based on stock kernel camera driver changes
* Package version: P607TUVSBPH1

Change-Id: I235954111d9d521d17820aaa34923b698f9f4792
Signed-off-by: Paul Keith <javelinanddart@gmail.com>

drivers/media/platform/msm/camera_ll/isp/msm_isp.h
@@ -421,6 +421,10 @@ struct vfe_device {
 	struct msm_isp_buf_mgr *buf_mgr;
 	int dump_reg;
 	uint32_t vfe_open_cnt;
+#ifdef CONFIG_MACH_LT03_LTE
+	uint32_t frame_id;
+	uint32_t eof_event_occur;
+#endif
 };
 
 #endif

drivers/media/platform/msm/camera_ll/isp/msm_isp40.c
@@ -283,8 +283,12 @@ static void msm_vfe40_process_reset_irq(struct vfe_device *vfe_dev,
 static void msm_vfe40_process_halt_irq(struct vfe_device *vfe_dev,
 	uint32_t irq_status0, uint32_t irq_status1)
 {
-	if (irq_status1 & (1 << 8))
+	if (irq_status1 & (1 << 8)) {
 		complete(&vfe_dev->halt_complete);
+#ifdef CONFIG_MACH_LT03_LTE
+		msm_camera_io_w(0x0, vfe_dev->vfe_base + 0x2C0);
+#endif
+	}
 }
 
 static void msm_vfe40_process_camif_irq(struct vfe_device *vfe_dev,
@@ -305,8 +309,12 @@ static void msm_vfe40_process_camif_irq(struct vfe_device *vfe_dev,
 			msm_isp_update_framedrop_reg(vfe_dev);
 		}
 	}
-	if (irq_status0 & (1 << 1))
+	if (irq_status0 & (1 << 1)) {
+#ifdef CONFIG_MACH_LT03_LTE
+		vfe_dev->eof_event_occur = 1;
+#endif
 		ISP_DBG("%s: EOF IRQ\n", __func__);
+	}
 	if (irq_status0 & (1 << 2))
 		ISP_DBG("%s: EPOCH0 IRQ\n", __func__);
 	if (irq_status0 & (1 << 3))

drivers/media/platform/msm/camera_ll/isp/msm_isp_axi_util.c
@@ -420,6 +420,10 @@ void msm_isp_sof_notify(struct vfe_device *vfe_dev,
 
 	sof_event.frame_id = vfe_dev->axi_data.src_info[frame_src].frame_id;
 	sof_event.timestamp = ts->event_time;
+#ifdef CONFIG_MACH_LT03_LTE
+	vfe_dev->frame_id = vfe_dev->axi_data.src_info[frame_src].frame_id;
+	vfe_dev->eof_event_occur = 0;
+#endif
 	msm_isp_send_event(vfe_dev, ISP_EVENT_SOF, &sof_event);
 }
 

drivers/media/platform/msm/camera_ll/sensor/csiphy/msm_csiphy.c
@@ -95,6 +95,9 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 		msm_camera_io_w(0x10, csiphybase + MIPI_CSIPHY_LNCK_CFG2_ADDR);
 		msm_camera_io_w(csiphy_params->settle_cnt,
 			 csiphybase + MIPI_CSIPHY_LNCK_CFG3_ADDR);
+#ifdef CONFIG_MACH_LT03_LTE
+		msm_camera_io_w(0xff, csiphybase + MIPI_CSIPHY_LNCK_CFG4_ADDR);
+#endif
 		msm_camera_io_w(0x24,
 			csiphybase + MIPI_CSIPHY_INTERRUPT_MASK0_ADDR);
 		msm_camera_io_w(0x24,
@@ -106,6 +109,10 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 		msm_camera_io_w(csiphy_params->combo_mode <<
 			MIPI_CSIPHY_MODE_CONFIG_SHIFT,
 			csiphybase + MIPI_CSIPHY_GLBL_RESET_ADDR);
+#ifdef CONFIG_MACH_LT03_LTE
+		msm_camera_io_w(0xff, csiphybase + MIPI_CSIPHY_LNCK_CFG4_ADDR);
+#endif
+
 	}
 
 	lane_mask &= 0x1f;
@@ -123,6 +130,12 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 			MIPI_CSIPHY_INTERRUPT_MASK_ADDR + 0x4*j);
 		msm_camera_io_w(MIPI_CSIPHY_INTERRUPT_MASK_VAL, csiphybase +
 			MIPI_CSIPHY_INTERRUPT_CLEAR_ADDR + 0x4*j);
+
+#ifdef CONFIG_MACH_LT03_LTE
+		if (csiphy_dev->hw_version == CSIPHY_VERSION_V3)
+			msm_camera_io_w(0xff, csiphybase + MIPI_CSIPHY_LNn_CFG4_ADDR+0x40*j);
+#endif
+
 		j++;
 		lane_mask >>= 1;
 	}

drivers/media/platform/msm/camera_ll/sensor/eeprom/msm_eeprom.c
@@ -246,11 +246,21 @@ static int msm_eeprom_match_id(struct msm_eeprom_ctrl_t *e_ctrl)
 	rc = msm_camera_spi_query_id(client, 0, &id[0], 2);
 	if (rc < 0)
 		return rc;
+#ifdef CONFIG_MACH_LT03_LTE
+	pr_info("%s: read 0x%x 0x%x, check 0:0x%x 0x%x 1:0x%x 0x%x\n", __func__,
+	id[0], id[1], client->spi_client->mfr_id0, client->spi_client->device_id0,
+	client->spi_client->mfr_id1, client->spi_client->device_id1);
+
+	if ((id[0] != client->spi_client->mfr_id0 || id[1] != client->spi_client->device_id0)
+	&& (id[0] != client->spi_client->mfr_id1 || id[1] != client->spi_client->device_id1))
+		return -ENODEV;
+#else
 	pr_info("%s: read 0x%x 0x%x, check 0x%x 0x%x\n", __func__, id[0],
 	     id[1], client->spi_client->mfr_id, client->spi_client->device_id);
 	if (id[0] != client->spi_client->mfr_id
 		    || id[1] != client->spi_client->device_id)
 		return -ENODEV;
+#endif
 	return 0;
 }
 /**
@@ -851,6 +861,23 @@ static int msm_eeprom_spi_parse_of(struct msm_camera_spi_client *spic)
 		return rc;
 	}
 	spic->erase_size = tmp[0];
+#ifdef CONFIG_MACH_LT03_LTE
+	rc = of_property_read_u32_array(of, "qcom,eeprom-id0", tmp, 2);
+	if (rc < 0) {
+		pr_err("%s: Failed to get eeprom id 0\n", __func__);
+		return rc;
+	}
+	spic->mfr_id0 = tmp[0];
+	spic->device_id0 = tmp[1];
+
+	rc = of_property_read_u32_array(of, "qcom,eeprom-id1", tmp, 2);
+	if (rc < 0) {
+		pr_err("%s: Failed to get eeprom id 1\n", __func__);
+		return rc;
+	}
+	spic->mfr_id1 = tmp[0];
+	spic->device_id1 = tmp[1];
+#else
 	rc = of_property_read_u32_array(of, "qcom,eeprom-id", tmp, 2);
 	if (rc < 0) {
 		pr_err("%s: Failed to get eeprom id\n", __func__);
@@ -858,6 +885,7 @@ static int msm_eeprom_spi_parse_of(struct msm_camera_spi_client *spic)
 	}
 	spic->mfr_id = tmp[0];
 	spic->device_id = tmp[1];
+#endif
 
 	return 0;
 }

drivers/media/platform/msm/camera_ll/sensor/msm_sensor_init.c
@@ -145,6 +145,25 @@ static ssize_t back_camera_firmware_store(struct device *dev,
 	return size;
 }
 
+#ifdef CONFIG_MACH_LT03_LTE
+char cam_fw_full_ver[40] = "NULL NULL NULL\n";//multi module
+static ssize_t back_camera_firmware_full_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+    CDBG("[FW_DBG] cam_fw_ver : %s\n", cam_fw_full_ver);
+    return snprintf(buf, sizeof(cam_fw_full_ver), "%s", cam_fw_full_ver);
+}
+
+static ssize_t back_camera_firmware_full_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+    CDBG("[FW_DBG] buf : %s\n", buf);
+    snprintf(cam_fw_full_ver, sizeof(cam_fw_full_ver), "%s", buf);
+
+    return size;
+}
+#endif
+
 char cam_load_fw[25] = "NULL\n";
 static ssize_t back_camera_firmware_load_show(struct device *dev,
 			struct device_attribute *attr, char *buf)
@@ -164,7 +183,11 @@ static ssize_t back_camera_firmware_load_store(struct device *dev,
 static ssize_t front_camera_firmware_show(struct device *dev,
 			struct device_attribute *attr, char *buf)
 {
+#ifdef CONFIG_MACH_LT03_LTE
+	char cam_fw[] = "S5K6B2YX N\n";
+#else
 	char cam_fw[] = "S5K6B2YX S5K6B2YX\n";
+#endif
 
 	return  snprintf(buf, sizeof(cam_fw), "%s", cam_fw);
 }
@@ -172,6 +195,8 @@ static ssize_t front_camera_firmware_show(struct device *dev,
 static DEVICE_ATTR(rear_camtype, S_IRUGO, back_camera_type_show, NULL);
 static DEVICE_ATTR(rear_camfw, S_IRUGO|S_IWUSR|S_IWGRP,
     back_camera_firmware_show, back_camera_firmware_store);
+static DEVICE_ATTR(rear_camfw_full, S_IRUGO | S_IWUSR | S_IWGRP,
+    back_camera_firmware_full_show, back_camera_firmware_full_store);
 static DEVICE_ATTR(rear_camfw_load, S_IRUGO|S_IWUSR|S_IWGRP,
     back_camera_firmware_load_show, back_camera_firmware_load_store);
 static DEVICE_ATTR(front_camtype, S_IRUGO, front_camera_type_show, NULL);
@@ -225,6 +250,12 @@ static int __init msm_sensor_init_module(void)
 		printk("Failed to create device file!(%s)!\n",
 			dev_attr_rear_camfw.attr.name);
 	}
+#ifdef CONFIG_MACH_LT03_LTE
+	if (device_create_file(cam_dev_back, &dev_attr_rear_camfw_full) < 0) {
+		printk("Failed to create device file!(%s)!\n",
+			dev_attr_rear_camfw_full.attr.name);
+	}
+#endif
 	if (device_create_file(cam_dev_back, &dev_attr_rear_camfw_load) < 0) {
 		printk("Failed to create device file!(%s)!\n",
 			dev_attr_rear_camfw_load.attr.name);

include/media/msmb_isp.h
@@ -272,7 +272,7 @@ enum msm_vfe_reg_cfg_type {
 struct msm_vfe_cfg_cmd2 {
 	uint16_t num_cfg;
 	uint16_t cmd_len;
-#ifdef CONFIG_MSMB_CAMERA_MM
+#if defined(CONFIG_MSMB_CAMERA_MM) || defined(CONFIG_MACH_LT03_LTE)
 	uint32_t frame_id;
 #endif
 	void __user *cfg_data;

pascua28/s5-kernel

drivers/media/platform/msm/camera_ll/isp/msm_isp.h
@@ -421,6 +421,10 @@ struct vfe_device {
 	struct msm_isp_buf_mgr *buf_mgr;
 	int dump_reg;
 	uint32_t vfe_open_cnt;
+#ifdef CONFIG_SEC_LT03_PROJECT
+	uint32_t frame_id;
+	uint32_t eof_event_occur;
+#endif
 };
 
 #endif

drivers/media/platform/msm/camera_ll/isp/msm_isp40.c
@@ -283,8 +283,12 @@ static void msm_vfe40_process_reset_irq(struct vfe_device *vfe_dev,
 static void msm_vfe40_process_halt_irq(struct vfe_device *vfe_dev,
 	uint32_t irq_status0, uint32_t irq_status1)
 {
-	if (irq_status1 & (1 << 8))
+	if (irq_status1 & (1 << 8)) {
 		complete(&vfe_dev->halt_complete);
+#ifdef CONFIG_SEC_LT03_PROJECT
+		msm_camera_io_w(0x0, vfe_dev->vfe_base + 0x2C0);
+#endif
+	}
 }
 
 static void msm_vfe40_process_camif_irq(struct vfe_device *vfe_dev,
@@ -305,8 +309,12 @@ static void msm_vfe40_process_camif_irq(struct vfe_device *vfe_dev,
 			msm_isp_update_framedrop_reg(vfe_dev);
 		}
 	}
-	if (irq_status0 & (1 << 1))
+	if (irq_status0 & (1 << 1)) {
+#ifdef CONFIG_SEC_LT03_PROJECT
+		vfe_dev->eof_event_occur = 1;
+#endif
 		ISP_DBG("%s: EOF IRQ\n", __func__);
+	}
 	if (irq_status0 & (1 << 2))
 		ISP_DBG("%s: EPOCH0 IRQ\n", __func__);
 	if (irq_status0 & (1 << 3))

drivers/media/platform/msm/camera_ll/isp/msm_isp_axi_util.c
@@ -420,6 +420,10 @@ void msm_isp_sof_notify(struct vfe_device *vfe_dev,
 
 	sof_event.frame_id = vfe_dev->axi_data.src_info[frame_src].frame_id;
 	sof_event.timestamp = ts->event_time;
+#ifdef CONFIG_SEC_LT03_PROJECT
+	vfe_dev->frame_id = vfe_dev->axi_data.src_info[frame_src].frame_id;
+	vfe_dev->eof_event_occur = 0;
+#endif
 	msm_isp_send_event(vfe_dev, ISP_EVENT_SOF, &sof_event);
 }
 

drivers/media/platform/msm/camera_ll/sensor/csiphy/msm_csiphy.c
@@ -95,6 +95,9 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 		msm_camera_io_w(0x10, csiphybase + MIPI_CSIPHY_LNCK_CFG2_ADDR);
 		msm_camera_io_w(csiphy_params->settle_cnt,
 			 csiphybase + MIPI_CSIPHY_LNCK_CFG3_ADDR);
+#ifdef CONFIG_SEC_LT03_PROJECT
+		msm_camera_io_w(0xff, csiphybase + MIPI_CSIPHY_LNCK_CFG4_ADDR);
+#endif
 		msm_camera_io_w(0x24,
 			csiphybase + MIPI_CSIPHY_INTERRUPT_MASK0_ADDR);
 		msm_camera_io_w(0x24,
@@ -106,6 +109,10 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 		msm_camera_io_w(csiphy_params->combo_mode <<
 			MIPI_CSIPHY_MODE_CONFIG_SHIFT,
 			csiphybase + MIPI_CSIPHY_GLBL_RESET_ADDR);
+#ifdef CONFIG_SEC_LT03_PROJECT
+		msm_camera_io_w(0xff, csiphybase + MIPI_CSIPHY_LNCK_CFG4_ADDR);
+#endif
+
 	}
 
 	lane_mask &= 0x1f;
@@ -123,6 +130,12 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 			MIPI_CSIPHY_INTERRUPT_MASK_ADDR + 0x4*j);
 		msm_camera_io_w(MIPI_CSIPHY_INTERRUPT_MASK_VAL, csiphybase +
 			MIPI_CSIPHY_INTERRUPT_CLEAR_ADDR + 0x4*j);
+
+#ifdef CONFIG_SEC_LT03_PROJECT
+		if (csiphy_dev->hw_version == CSIPHY_VERSION_V3)
+			msm_camera_io_w(0xff, csiphybase + MIPI_CSIPHY_LNn_CFG4_ADDR+0x40*j);
+#endif
+
 		j++;
 		lane_mask >>= 1;
 	}

drivers/media/platform/msm/camera_ll/sensor/eeprom/msm_eeprom.c
@@ -246,11 +246,21 @@ static int msm_eeprom_match_id(struct msm_eeprom_ctrl_t *e_ctrl)
 	rc = msm_camera_spi_query_id(client, 0, &id[0], 2);
 	if (rc < 0)
 		return rc;
+#ifdef CONFIG_SEC_LT03_PROJECT
+	pr_info("%s: read 0x%x 0x%x, check 0:0x%x 0x%x 1:0x%x 0x%x\n", __func__,
+	id[0], id[1], client->spi_client->mfr_id0, client->spi_client->device_id0,
+	client->spi_client->mfr_id1, client->spi_client->device_id1);
+
+	if ((id[0] != client->spi_client->mfr_id0 || id[1] != client->spi_client->device_id0)
+	&& (id[0] != client->spi_client->mfr_id1 || id[1] != client->spi_client->device_id1))
+		return -ENODEV;
+#else
 	pr_info("%s: read 0x%x 0x%x, check 0x%x 0x%x\n", __func__, id[0],
 	     id[1], client->spi_client->mfr_id, client->spi_client->device_id);
 	if (id[0] != client->spi_client->mfr_id
 		    || id[1] != client->spi_client->device_id)
 		return -ENODEV;
+#endif
 	return 0;
 }
 /**
@@ -851,6 +861,23 @@ static int msm_eeprom_spi_parse_of(struct msm_camera_spi_client *spic)
 		return rc;
 	}
 	spic->erase_size = tmp[0];
+#ifdef CONFIG_SEC_LT03_PROJECT
+	rc = of_property_read_u32_array(of, "qcom,eeprom-id0", tmp, 2);
+	if (rc < 0) {
+		pr_err("%s: Failed to get eeprom id 0\n", __func__);
+		return rc;
+	}
+	spic->mfr_id0 = tmp[0];
+	spic->device_id0 = tmp[1];
+
+	rc = of_property_read_u32_array(of, "qcom,eeprom-id1", tmp, 2);
+	if (rc < 0) {
+		pr_err("%s: Failed to get eeprom id 1\n", __func__);
+		return rc;
+	}
+	spic->mfr_id1 = tmp[0];
+	spic->device_id1 = tmp[1];
+#else
 	rc = of_property_read_u32_array(of, "qcom,eeprom-id", tmp, 2);
 	if (rc < 0) {
 		pr_err("%s: Failed to get eeprom id\n", __func__);
@@ -858,6 +885,7 @@ static int msm_eeprom_spi_parse_of(struct msm_camera_spi_client *spic)
 	}
 	spic->mfr_id = tmp[0];
 	spic->device_id = tmp[1];
+#endif
 
 	return 0;
 }

drivers/media/platform/msm/camera_ll/sensor/msm_sensor_init.c
@@ -145,6 +145,25 @@ static ssize_t back_camera_firmware_store(struct device *dev,
 	return size;
 }
 
+#ifdef CONFIG_SEC_LT03_PROJECT
+char cam_fw_full_ver[40] = "NULL NULL NULL\n";//multi module
+static ssize_t back_camera_firmware_full_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+    CDBG("[FW_DBG] cam_fw_ver : %s\n", cam_fw_full_ver);
+    return snprintf(buf, sizeof(cam_fw_full_ver), "%s", cam_fw_full_ver);
+}
+
+static ssize_t back_camera_firmware_full_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+    CDBG("[FW_DBG] buf : %s\n", buf);
+    snprintf(cam_fw_full_ver, sizeof(cam_fw_full_ver), "%s", buf);
+
+    return size;
+}
+#endif
+
 char cam_load_fw[25] = "NULL\n";
 static ssize_t back_camera_firmware_load_show(struct device *dev,
 			struct device_attribute *attr, char *buf)
@@ -164,7 +183,11 @@ static ssize_t back_camera_firmware_load_store(struct device *dev,
 static ssize_t front_camera_firmware_show(struct device *dev,
 			struct device_attribute *attr, char *buf)
 {
+#ifdef CONFIG_SEC_LT03_PROJECT
+	char cam_fw[] = "S5K6B2YX N\n";
+#else
 	char cam_fw[] = "S5K6B2YX S5K6B2YX\n";
+#endif
 
 	return  snprintf(buf, sizeof(cam_fw), "%s", cam_fw);
 }
@@ -172,6 +195,10 @@ static ssize_t front_camera_firmware_show(struct device *dev,
 static DEVICE_ATTR(rear_camtype, S_IRUGO, back_camera_type_show, NULL);
 static DEVICE_ATTR(rear_camfw, S_IRUGO|S_IWUSR|S_IWGRP,
     back_camera_firmware_show, back_camera_firmware_store);
+#ifdef CONFIG_SEC_LT03_PROJECT
+static DEVICE_ATTR(rear_camfw_full, S_IRUGO | S_IWUSR | S_IWGRP,
+    back_camera_firmware_full_show, back_camera_firmware_full_store);
+#endif
 static DEVICE_ATTR(rear_camfw_load, S_IRUGO|S_IWUSR|S_IWGRP,
     back_camera_firmware_load_show, back_camera_firmware_load_store);
 static DEVICE_ATTR(front_camtype, S_IRUGO, front_camera_type_show, NULL);
@@ -225,6 +252,12 @@ static int __init msm_sensor_init_module(void)
 		printk("Failed to create device file!(%s)!\n",
 			dev_attr_rear_camfw.attr.name);
 	}
+#ifdef CONFIG_SEC_LT03_PROJECT
+	if (device_create_file(cam_dev_back, &dev_attr_rear_camfw_full) < 0) {
+		printk("Failed to create device file!(%s)!\n",
+			dev_attr_rear_camfw_full.attr.name);
+	}
+#endif
 	if (device_create_file(cam_dev_back, &dev_attr_rear_camfw_load) < 0) {
 		printk("Failed to create device file!(%s)!\n",
 			dev_attr_rear_camfw_load.attr.name);

include/media/msmb_isp.h
@@ -272,7 +272,7 @@ enum msm_vfe_reg_cfg_type {
 struct msm_vfe_cfg_cmd2 {
 	uint16_t num_cfg;
 	uint16_t cmd_len;
-#ifdef CONFIG_MSMB_CAMERA_MM
+#if defined(CONFIG_MSMB_CAMERA_MM) || defined(CONFIG_SEC_LT03_PROJECT)
 	uint32_t frame_id;
 #endif
 	void __user *cfg_data;