LineageOS/android_kernel_samsung_msm8974

drivers: media: platform: msm: camera_ll: Add lt03lte compatibility
* Largely based on stock kernel camera driver changes
* Package version: P607TUVSBPH1

Change-Id: I235954111d9d521d17820aaa34923b698f9f4792
Signed-off-by: Paul Keith <javelinanddart@gmail.com>

drivers/media/platform/msm/camera_ll/msm.c
@@ -29,6 +29,7 @@
 #include "msm.h"
 #include "msm_vb2.h"
 #include "msm_sd.h"
+#include <media/msmb_generic_buf_mgr.h>
 
 static struct v4l2_device *msm_v4l2_dev;
 static struct list_head    ordered_sd_list;
@@ -529,6 +530,9 @@ static void msm_remove_session_cmd_ack_q(struct msm_session *session)
 int msm_destroy_session(unsigned int session_id)
 {
 	struct msm_session *session;
+#ifdef CONFIG_MACH_LT03_LTE
+	struct v4l2_subdev *buf_mgr_subdev;
+#endif
 
 	session = msm_queue_find(msm_session_q, struct msm_session,
 		list, __msm_queue_find_session, &session_id);
@@ -542,6 +546,17 @@ int msm_destroy_session(unsigned int session_id)
 	mutex_destroy(&session->lock);
 	msm_delete_entry(msm_session_q, struct msm_session,
 		list, session);
+
+#ifdef CONFIG_MACH_LT03_LTE
+	// Qualcomm patch
+	buf_mgr_subdev = msm_buf_mngr_get_subdev();
+	if (buf_mgr_subdev) {
+		v4l2_subdev_call(buf_mgr_subdev, core, ioctl,
+                            MSM_SD_SHUTDOWN, NULL);
+	} else {
+		pr_err("%s: Buff manger device node is NULL\n", __func__);
+	}
+#endif
 
 	pr_warn("%s : Succeed", __func__);
 

pascua28/s5-kernel

drivers/media/platform/msm/camera_ll/msm.c
@@ -29,6 +29,7 @@
 #include "msm.h"
 #include "msm_vb2.h"
 #include "msm_sd.h"
+#include <media/msmb_generic_buf_mgr.h>
 
 static struct v4l2_device *msm_v4l2_dev;
 static struct list_head    ordered_sd_list;
@@ -524,6 +525,9 @@ static void msm_remove_session_cmd_ack_q(struct msm_session *session)
 int msm_destroy_session(unsigned int session_id)
 {
 	struct msm_session *session;
+#ifdef CONFIG_SEC_LT03_PROJECT
+	struct v4l2_subdev *buf_mgr_subdev;
+#endif
 
 	session = msm_queue_find(msm_session_q, struct msm_session,
 		list, __msm_queue_find_session, &session_id);
@@ -537,6 +541,17 @@ int msm_destroy_session(unsigned int session_id)
 	mutex_destroy(&session->lock);
 	msm_delete_entry(msm_session_q, struct msm_session,
 		list, session);
+
+#ifdef CONFIG_SEC_LT03_PROJECT
+	// Qualcomm patch
+	buf_mgr_subdev = msm_buf_mngr_get_subdev();
+	if (buf_mgr_subdev) {
+		v4l2_subdev_call(buf_mgr_subdev, core, ioctl,
+                            MSM_SD_SHUTDOWN, NULL);
+	} else {
+		pr_err("%s: Buff manger device node is NULL\n", __func__);
+	}
+#endif
 
 	pr_warn("%s : Succeed", __func__);
 