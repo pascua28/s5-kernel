132564f709e0f181980cd7491ca49fddac5c501f
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-10-04 09:29:35 +0800

    build: set default CPU governor to intelliactive

--- build/ramdisk/overlay.d/intellikernel.sh
+++ build/ramdisk/overlay.d/intellikernel.sh
@@ -16,4 +16,7 @@ echo '0' > /sys/block/mmcblk0/queue/iostats
 echo '2048' > /sys/block/mmcblk1/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk1/queue/iostats
 
+echo 'intelliactive' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo '2265600' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+
 setenforce 1
