3139e1125f333daa9dac5dfa47c099db9e178ccb
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-10-04 19:02:47 +0800

    crypto: fix GCC warning

--- crypto/testmgr.h
+++ crypto/testmgr.h
@@ -15289,7 +15289,7 @@ static struct comp_testvec deflate_decomp_tv_template[] = {
 #define ZLIB_COMP_TEST_VECTORS 2
 #define ZLIB_DECOMP_TEST_VECTORS 2
 
-static const struct {
+static struct {
 	struct nlattr nla;
 	int val;
 } deflate_comp_params[] = {
@@ -15326,7 +15326,7 @@ static const struct {
 	}
 };
 
-static const struct {
+static struct {
 	struct nlattr nla;
 	int val;
 } deflate_decomp_params[] = {
