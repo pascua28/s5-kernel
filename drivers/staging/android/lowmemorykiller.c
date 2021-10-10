af74e1d547b0e0b44d1e112a2180af4e95944ee6
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-10-10 13:06:48 +0800

    Revert "android/lowmemorykiller: Ignore tasks with freed mm"
    This reverts commit 83af6936d0c1620565eaf875b1a242561a8aa54b.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -114,10 +114,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		if (tsk->flags & PF_KTHREAD)
 			continue;
 
-		/* if task no longer has any memory ignore it */
-		if (test_task_flag(tsk, TIF_MM_RELEASED))
-			continue;
-
 		p = find_lock_task_mm(tsk);
 		if (!p)
 			continue;
@@ -158,13 +154,10 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		send_sig(SIGKILL, selected, 0);
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		rem -= selected_tasksize;
-		rcu_read_unlock();
-		/* give the system time to free up the memory */
-		msleep_interruptible(20);
-	} else
-		rcu_read_unlock();
+	}
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
+	rcu_read_unlock();
 	return rem;
 }
 
