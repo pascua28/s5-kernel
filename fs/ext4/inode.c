359422ee8a806512c6102bf8aa0417ee575bd051
Samuel Pascua <sgpascua@ngcp.ph>
2022-01-05 07:58:08 +0000

    Revert "ext4 crypto: add ext4_mpage_readpages()"
    This reverts commit 2737ccf4553044fbc482a88c841a9c961abe03b1.

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -2821,7 +2821,7 @@ static int ext4_readpage(struct file *file, struct page *page)
 		ret = ext4_readpage_inline(inode, page);
 
 	if (ret == -EAGAIN)
-		return ext4_mpage_readpages(page->mapping, NULL, page, 1);
+		return mpage_readpage(page, ext4_get_block);
 
 	return ret;
 }
@@ -2836,7 +2836,7 @@ ext4_readpages(struct file *file, struct address_space *mapping,
 	if (ext4_has_inline_data(inode))
 		return 0;
 
-	return ext4_mpage_readpages(mapping, pages, NULL, nr_pages);
+	return mpage_readpages(mapping, pages, nr_pages, ext4_get_block);
 }
 
 static void ext4_invalidatepage(struct page *page, unsigned int offset,
