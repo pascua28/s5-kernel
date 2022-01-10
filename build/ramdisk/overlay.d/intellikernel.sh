fc87ddb2364751a2da7f936c9558364a0f9ee39f
Samuel Pascua <sgpascua@ngcp.ph>
2022-01-10 00:41:10 +0000

    Revert "cleanup script"
    This reverts commit 56cd143a3556731eede2abd3beb0864a9116192e.

--- build/ramdisk/overlay.d/intellikernel.sh
+++ build/ramdisk/overlay.d/intellikernel.sh
@@ -9,9 +9,18 @@ sysctl -w vm.swappiness=130
 # Disable adaptive lmk
 echo '0' > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 
+# SafetyNet
+chmod 640 /sys/fs/selinux/enforce;
+chmod 440 /sys/fs/selinux/policy;
+
 # Set I/O scheduler back to CFQ
-echo 'cfq' > /sys/block/mmcblk0/queue/scheduler
+setprop sys.io.scheduler cfq
+echo '1024' > /sys/block/mmcblk0/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk0/queue/iostats
+echo '2048' > /sys/block/mmcblk1/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk1/queue/iostats
+
+echo 'intelliactive' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo '2265600' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
 setenforce 1
